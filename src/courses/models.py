d3137a5d9646bac2ad0781dfd9ca924138ffcf54
Fedor Borshev <fedor@borshev.com>
2019-10-28 18:31:24 +0300

    Record.get_url()

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,4 +1,5 @@
 from app.models import TimestampedModel, models
+from app.s3 import AppS3
 
 
 class Course(TimestampedModel):
@@ -12,3 +13,6 @@ class Record(TimestampedModel):
     slug = models.SlugField()
 
     s3_object_id = models.CharField(max_length=512)
+
+    def get_url(self, expires: int = 60 * 60):
+        return AppS3().get_presigned_url(self.s3_object_id, expires=expires)
