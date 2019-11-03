6f4c09c2a641d8eef44e3c1fe4e0bccf86cef836
Fedor Borshev <fedor@borshev.com>
2019-11-03 16:55:22 +0300

    Mailjet & templowl (#1)

---
+++ src/app/mail/owl.py
@@ -0,0 +1,38 @@
+from typing import List, Union
+
+from django.conf import settings
+from django.core.mail import EmailMessage
+
+from app.mail import helpers
+
+
+class TemplOwl:
+    """This is a clone of the Owl class, with the same syntax, but supporting only template mailprovider messages.
+    This class is not compatible for providers without templates, such as mailgun"""
+    def __init__(self, to: Union[List, str], template_id, subject: str = '', ctx: dict = None):
+        if to in (None, [None]):
+            to = []
+        self.template_id = template_id
+        self.subject = subject
+        self.to = [to] if isinstance(to, str) else to
+        self.ctx = helpers.normalize_email_context(ctx) if ctx is not None else dict()
+        self.msg = self._get_message()
+
+    def _get_message(self) -> EmailMessage:
+        msg = EmailMessage(
+            subject=self.subject,
+            body='',
+            to=self.to,
+        )
+        msg.from_email = None  # force default mailprovider from email
+        msg.template_id = self.template_id
+        msg.merge_global_data = self.ctx
+        return msg
+
+    def send(self):
+        if settings.EMAIL_ENABLED:
+            self.msg.send()
+
+    def attach(self, filename=None, content=None, mimetype=None):
+        """Add an attachment to the message"""
+        return self.msg.attach(filename, content, mimetype)
---
+++ src/app/tests/owl/tests_own_functional.py
@@ -0,0 +1,42 @@
+import pytest
+from django.core import mail
+
+from app.mail.owl import TemplOwl
+
+
+@pytest.fixture(autouse=True)
+def adjust_settings(settings):
+    settings.EMAIL_ENABLED = True
+
+
+@pytest.fixture
+def owl():
+    return TemplOwl(
+        to='f@f213.in',
+        template_id=100500,
+    )
+
+
+def test_sending(owl):
+    owl.send()
+
+    assert len(mail.outbox) == 1
+
+
+@pytest.mark.parametrize('switch', [
+    # lambda settings: setattr(settings, 'DISABLE_NOTIFICATIONS', True),
+    lambda settings: setattr(settings, 'EMAIL_ENABLED', False),
+])
+def test_kill_switch(owl, switch, settings):
+    switch(settings)
+
+    owl.send()
+
+    assert len(mail.outbox) == 0
+
+
+def test_attaching(owl):
+    owl.attach(filename='testing_file_name_100500.txt', content=b'just testing')
+
+    assert len(owl.msg.attachments) == 1
+    assert 'testing_file_name_100500.txt' in owl.msg.attachments[0]
