f06b660b4cc9d4d513b262ecbb3cc08407e1b1de
Fedor Borshev <fedor@borshev.com>
2019-11-04 20:50:32 +0300

    Celery & sentry (#2)

--- src/app/settings.py
+++ src/app/settings.py
@@ -132,6 +132,23 @@ SUIT_CONFIG = {
     'ADMIN_NAME': 'myapp secret place',
 }
 
+SENTRY_DSN = env('SENTRY_DSN', cast=str, default='')
+
+if not DEBUG and len(SENTRY_DSN):
+    import sentry_sdk
+    from sentry_sdk.integrations.django import DjangoIntegration
+
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+    )
+
+BROKER_URL = env('CELERY_BACKEND')
+CELERY_ALWAYS_EAGER = env('CELERY_ALWAYS_EAGER', cast=bool, default=DEBUG)  # by default in debug mode we run all celery tasks in foregroud.
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_ENABLE_UTC = False
+
+
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', default=None)
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', default=None)
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default=None)
