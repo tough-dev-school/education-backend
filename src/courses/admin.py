cfe581b4455603c03a2dbec791d7f3a3c06b4b02
Fedor Borshev <fedor@borshev.com>
2019-11-05 23:40:18 +0300

    Added a downloadable link to the course admin

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,19 +1,33 @@
-from app.admin import ModelAdmin, StackedInline, admin
+from django.utils.html import format_html
+from django.utils.translation import ugettext_lazy as _
+
+from app.admin import ModelAdmin, TabularInline, admin
 from courses.models import Course, Record
 
 
-class RecordAdmin(StackedInline):
+class RecordAdmin(TabularInline):
     model = Record
 
     fields = [
         'name',
         'slug',
         's3_object_id',
+        'downloadable',
+    ]
+    readonly_fields = [
+        'downloadable',
     ]
 
-    display_fields = fields
     prepopulated_fields = {'slug': ('name',)}
     extra = 0
+
+    def downloadable(self, obj):
+        if obj is None or obj.s3_object_id is None or not len(obj.s3_object_id):
+            return '—'
+
+        return format_html('<a href="{}">Копировать</a>', obj.get_url())
+
+    downloadable.short_description = _('Downloadable link')
 
 
 @admin.register(Course)
