41e3f3d995d0cfbcc049a93e09f0d6743b01aa91
Fedor Borshev <fedor@borshev.com>
2019-11-13 17:52:26 +0300

    Disabled session auth for DRF

--- src/app/settings.py
+++ src/app/settings.py
@@ -96,7 +96,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': [
