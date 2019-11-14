b8d8aba158285616ca9cf85930250add5556f765
Fedor Borshev <fedor@borshev.com>
2019-11-14 15:56:24 +0300

    Introducing fancy admin decorators -- @action and @field

--- src/app/admin/__init__.py
+++ src/app/admin/__init__.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
+from app.admin.decorators import action, field
 from app.admin.model_admin import ModelAdmin, StackedInline, TabularInline
 
 __all__ = [
+    action,
     admin,
+    field,
+    ModelAdmin,
     StackedInline,
     TabularInline,
-    ModelAdmin,
 ]
---
+++ src/app/admin/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+
+def action(short_description=None):
+    """Shortcut for setting actions with short description. Does not add it to actions, one has to do it manualy"""
+    def decorator(fn):
+        @wraps(fn)
+        def decorated(*args, **kwargs):
+            return fn(*args, **kwargs)
+
+        if short_description is not None:
+            decorated.short_description = short_description
+
+        return decorated
+
+    return decorator
+
+
+def field(short_description=None, admin_order_field=None):
+    def decorator(fn):
+        @wraps(fn)
+        def decorated(*args, **kwargs):
+
+            return fn(*args, **kwargs)
+
+        if short_description is not None:
+            decorated.short_description = short_description
+
+        if admin_order_field is not None:
+            decorated.admin_order_field = admin_order_field
+
+        return decorated
+
+    return decorator
