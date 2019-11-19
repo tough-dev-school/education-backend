43e71bba185beeee7906f990842a90046418e4cb
Fedor Borshev <fedor@borshev.com>
2019-11-19 12:19:24 +0300

    Added DRF tracking

--- src/app/settings.py
+++ src/app/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'rest_framework_tracking',
 
     'suit',
     'django.contrib.admin',
--- src/tinkoff/api/views.py
+++ src/tinkoff/api/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
+from rest_framework_tracking.mixins import LoggingMixin
 
 from app.views import AnonymousAPIView
 from tinkoff.api.serializers import PaymentNotificationSerializer
 
 
-class TinkoffPaymentNotificationsView(AnonymousAPIView):
+class TinkoffPaymentNotificationsView(LoggingMixin, AnonymousAPIView):
     def post(self, request, *args, **kwargs):
         serializer = PaymentNotificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
