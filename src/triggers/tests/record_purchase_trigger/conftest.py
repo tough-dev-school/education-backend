8155ea59ec42f24860e407aefbc4e051d38e0591
Alexandr Popov <grepto@gmail.com>
2019-12-29 16:40:55 +0700

    fix ordering for fixtures order and trigger

--- src/triggers/tests/record_purchase_trigger/conftest.py
+++ src/triggers/tests/record_purchase_trigger/conftest.py
@@ -8,13 +8,13 @@ pytestmark = [pytest.mark.django_db]
 
 
 @pytest.fixture
-def trigger(order):
-    return RecordPurchaseTrigger(order)
-
-
-@pytest.fixture
 def order(order, record):
     order.set_item(record)
     order.setattr_and_save('paid', datetime(2032, 12, 1, 15, 13))
 
     return order
+
+
+@pytest.fixture
+def trigger(order):
+    return RecordPurchaseTrigger(order)
