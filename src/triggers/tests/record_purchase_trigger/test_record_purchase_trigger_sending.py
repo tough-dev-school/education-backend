6dec2b6bc9e6694093bfcc46b4081b74c4314ecc
Alexandr Popov <grepto@gmail.com>
2019-12-29 16:32:55 +0700

    add tests for check sending mail

---
+++ src/triggers/tests/record_purchase_trigger/test_record_purchase_trigger_sending.py
@@ -0,0 +1,29 @@
+import pytest
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture
+def send(mocker):
+    return mocker.patch('app.tasks.send_mail.delay')
+
+
+@pytest.fixture(autouse=True)
+def condition(mocker, trigger):
+    return mocker.patch.object(trigger, 'condition', return_value=True)
+
+
+def test(trigger, send):
+    trigger()
+
+    args = send.call_args[1]
+
+    assert args['ctx']['firstname'] == 'Камаз'
+    assert args['to'] == 'kamaz.otkhodov@gmail.com'
+
+
+def test_only_once(trigger, send):
+    for _ in range(0, 2):
+        trigger()
+
+    send.assert_called_once()
