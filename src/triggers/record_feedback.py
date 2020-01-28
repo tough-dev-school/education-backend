d3ea7e9f447756cfe088424515ac8dd5aa91d241
Alexandr Popov <grepto@gmail.com>
2020-01-28 17:36:30 +0300

    add classmethod run for getting relevant orders and run trigger for them

--- src/triggers/record_feedback.py
+++ src/triggers/record_feedback.py
@@ -2,14 +2,23 @@ from datetime import timedelta
 
 from django.utils import timezone
 
+from factory import register
+from orders.models import Order
+from tasks import run_trigger
 from triggers.base import BaseTrigger
 
 
+@register('record_feedback')
 class RecordFeedbackTrigger(BaseTrigger):
     name = 'record_feedback'
     template_id = 1167476
 
     PERIOD = timedelta(days=3)
+
+    @classmethod
+    def run(cls):
+        for order in Order.objects.filter(paid__isnull=False, record__isnull=False, created__gte=timezone.now() - timedelta(days=6)).iterator():
+            run_trigger.delay(cls.name, order.pk)
 
     def condition(self):
         """Order should be paid, item should be a record and was created more then three days ago
