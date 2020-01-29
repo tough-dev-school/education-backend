9971b11ef21a28ff4a7c4759b97f459f81400aad
Alexandr Popov <grepto@gmail.com>
2020-01-29 14:56:17 +0300

    delete damaged test

--- src/triggers/tests/triggers_factory/tests_triggers_factory_get_all_triggers.py
+++ src/triggers/tests/triggers_factory/tests_triggers_factory_get_all_triggers.py
@@ -8,8 +8,3 @@ pytestmark = [pytest.mark.django_db]
 def test_specific_trigger_in_get_all_triggers(test_trigger):
 
     assert test_trigger in factory.get_all_triggers()
-
-
-def test_count_triggers_in_get_all_triggers():
-
-    assert len(factory.get_all_triggers()) == 1
