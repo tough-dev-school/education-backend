311750c556d165e2ae6da4c92bff17c83d3d4933
Alexandr Popov <grepto@gmail.com>
2020-01-29 13:51:55 +0300

    remove RecordFeedbackTrigger from tests

--- src/triggers/tests/triggers_factory/tests_triggers_factory_get.py
+++ src/triggers/tests/triggers_factory/tests_triggers_factory_get.py
@@ -1,16 +1,15 @@
 import pytest
 
 from triggers import factory
-from triggers.record_feedback import RecordFeedbackTrigger
 
 pytestmark = [pytest.mark.django_db]
 
-def test_record_feedback_trigger():
-    assert factory.get(RecordFeedbackTrigger.name) == RecordFeedbackTrigger
+
+def test_get_registered_trigger(test_trigger):
+    assert factory.get('test') == test_trigger
 
 
 def test_non_found():
     """Trigger without a registered in registry"""
     with pytest.raises(factory.TriggerNotRegistered):
         factory.get('supper dupper class')
-
