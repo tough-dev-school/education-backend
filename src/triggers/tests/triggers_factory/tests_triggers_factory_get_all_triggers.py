2a246d67d00fed92eae2771358443df899a7f5e9
Alexandr Popov <grepto@gmail.com>
2020-01-29 13:52:27 +0300

    remove RecordFeedbackTrigger from tests

--- src/triggers/tests/triggers_factory/tests_triggers_factory_get_all_triggers.py
+++ src/triggers/tests/triggers_factory/tests_triggers_factory_get_all_triggers.py
@@ -1,17 +1,15 @@
 import pytest
 
 from triggers import factory
-from triggers.record_feedback import RecordFeedbackTrigger
 
 pytestmark = [pytest.mark.django_db]
 
 
-def test_specific_trigger_in_get_all_triggers():
+def test_specific_trigger_in_get_all_triggers(test_trigger):
 
-    assert RecordFeedbackTrigger in factory.get_all_triggers()
+    assert test_trigger in factory.get_all_triggers()
 
 
 def test_count_triggers_in_get_all_triggers():
-    REGISTERED_TRIGGERS_COUNT = 1
 
-    assert len(factory.get_all_triggers()) == REGISTERED_TRIGGERS_COUNT
+    assert len(factory.get_all_triggers()) == 1
