c831e1545b9c384652f130964b9c61d9763a432a
Alexandr Popov <grepto@gmail.com>
2020-01-29 14:58:44 +0300

    return tasks for started_purchase trigger

--- src/triggers/tasks.py
+++ src/triggers/tasks.py
@@ -1,6 +1,24 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
 from app.celery import celery
-from orders.models import Order
 from triggers import factory
+from orders.models import Order
+from triggers.started_purchase import StartedPurchaseTrigger
+
+
+@celery.task
+def run_started_purchase_trigger(order_id):
+    order = Order.objects.get(pk=order_id)
+
+    StartedPurchaseTrigger(order)()
+
+
+@celery.task
+def check_for_started_purchase_triggers():
+    for order in Order.objects.filter(paid__isnull=True, created__gte=timezone.now() - timedelta(days=3)).iterator():
+        run_started_purchase_trigger.delay(order.pk)
 
 
 @celery.task
