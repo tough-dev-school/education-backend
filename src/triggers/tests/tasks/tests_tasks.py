d9f00063b3a5b1f2db4f968acf7e34f5ca5e3dea
grepto <grepto@gmail.com>
2020-03-01 12:28:49 +0300

    Check call params for run_trigger in test_runner_all_triggers_task

--- src/triggers/tests/tasks/tests_tasks.py
+++ src/triggers/tests/tasks/tests_tasks.py
@@ -16,15 +16,22 @@ def find_orders(mocker, test_trigger):
 
 
 @pytest.fixture
+def trigger_task(mocker):
+    return mocker.patch('triggers.tasks.run_trigger.delay')
+
+
+@pytest.fixture
 def execute_result(mocker, test_trigger):
     return mocker.patch.object(test_trigger, '__call__')
 
 
-def test_runner_all_triggers_task(get_all_triggers_result, find_orders, test_trigger):
+def test_runner_all_triggers_task(get_all_triggers_result, find_orders, test_trigger, trigger_task, order):
     get_all_triggers_result.return_value = [test_trigger]
+    find_orders.return_value = [order.pk]
     run_all_triggers()
 
     find_orders.assert_called_once()
+    trigger_task.assert_called_once_with('test', order.pk)
 
 
 def test_run_trigger_task(order, test_trigger, execute_result):
