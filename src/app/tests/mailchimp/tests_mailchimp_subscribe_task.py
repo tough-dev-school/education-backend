6dc52034c1d429419ab0d2be89e20f0cd02f2da1
Fedor Borshev <fedor@borshev.com>
2020-09-27 14:44:28 +0300

    Disabled using mailchimp during tests

--- src/app/tests/mailchimp/tests_mailchimp_subscribe_task.py
+++ src/app/tests/mailchimp/tests_mailchimp_subscribe_task.py
@@ -14,3 +14,11 @@ def test_task(subscribe, user):
     tasks.subscribe_to_mailchimp.delay(user.pk)
 
     subscribe.assert_called_once_with(user)
+
+
+def test_task_does_not_do_things_if_there_is_no_mailchimp_contact_list_id(subscribe, user, settings):
+    settings.MAILCHIMP_CONTACT_LIST_ID = None
+
+    tasks.subscribe_to_mailchimp.delay(user.pk)
+
+    subscribe.assert_not_called()
