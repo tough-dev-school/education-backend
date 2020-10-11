c6793671517bf99e39e74e6adb6ce364504ade91
Fedor Borshev <fedor@borshev.com>
2020-10-11 11:45:20 +0300

    Some fancy translations

--- src/magnets/apps.py
+++ src/magnets/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class MagnetsConfig(AppConfig):
     name = 'magnets'
+
+    verbose_name = _('Magnets')
--- src/users/apps.py
+++ src/users/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class UsersConfig(AppConfig):
     name = 'users'
+    verbose_name = _('Users')
