69c88a743b7d71361e84025ea9e979ae27cbff67
Fedor Borshev <fedor@borshev.com>
2020-10-23 21:47:28 +0300

    admin: creating users with emails only

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,16 +1,39 @@
+from django import forms
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
 from app.admin import admin
+from users.creator import UserCreator
 from users.models import User
 
 admin.site.unregister(Group)
 
 
+class PasswordLessUserCreationForm(forms.ModelForm):
+    email = forms.CharField()
+
+    class Meta:
+        model = User
+        fields = [
+            'email',
+        ]
+
+    def save_m2m(self, *args, **kwargs):
+        pass
+
+    def save(self, commit=True):
+        return UserCreator(name='', email=self.cleaned_data['email'])()
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    pass
+    add_form = PasswordLessUserCreationForm
+    add_fieldsets = (
+        (None, {
+            'fields': ['email'],
+        }),
+    )
 
 
 @admin.register(Group)
