8aecf41cbd7593f4f02ccc4f2c7455efd32e05c0
Fedor Borshev <fedor@borshev.com>
2020-12-17 21:47:36 +0300

    Dropped djangorestframework-jwt cuz we dont use jwt here

--- src/app/settings.py
+++ src/app/settings.py
@@ -110,7 +110,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': [
