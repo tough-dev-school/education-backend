08fb830771a050bd16c9ba9c69ec88f76d239d82
Fedor Borshev <fedor@borshev.com>
2020-12-25 18:59:45 +0300

    Dropped django-suit remaingins

--- src/courses/static/js/admin/course_action_form.js
+++ src/courses/static/js/admin/course_action_form.js
@@ -1,14 +1,16 @@
-$(document).ready(() => {
-  var $templateId = $(".actions input[name=template_id]");
-  var $action = $(".actions select[name=action]");
+(function ($) {
+  $(document).ready(() => {
+    var $templateId = $(".actions input[name=template_id]");
+    var $action = $(".actions select[name=action]");
 
-  $templateId.css("display", "None");
-
-  $action.change((e) => {
-    if (e.target.value == "send_email_to_all_purchased_users") {
-      $templateId.css("display", "inline-block");
-      return;
-    }
     $templateId.css("display", "None");
+
+    $action.change((e) => {
+      if (e.target.value == "send_email_to_all_purchased_users") {
+        $templateId.css("display", "inline-block");
+        return;
+      }
+      $templateId.css("display", "None");
+    });
   });
-});
+})(django.jQuery);
