3c37a22aae720e8dc6a02988fd3b577110376884
Fedor Borshev <fedor@borshev.com>
2020-12-31 13:59:30 +0300

    Quick fix to make mixer work with new version of faker

--- src/users/tests/tests_user_str.py
+++ src/users/tests/tests_user_str.py
@@ -7,9 +7,14 @@ pytestmark = [pytest.mark.django_db]
     ['Kamaz', 'Othodov', 'Kamaz Othodov'],
     ['Kamaz', '', 'Kamaz'],
     ['', 'Othodov', 'Othodov'],
-    ['', '', 'zerocool'],
 ])
 def test(mixer, first_name, last_name, expected):
-    user = mixer.blend('users.User', username='zerocool', first_name=first_name, last_name=last_name)
+    user = mixer.blend('users.User', first_name=first_name, last_name=last_name)
 
     assert str(user) == expected
+
+
+def test_username_is_used_by_default(mixer):
+    user = mixer.blend('users.User', first_name='', last_name='')
+
+    assert str(user) == user.username
