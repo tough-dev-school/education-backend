4920064a98dcc3ee2f4c42759545938258269551
Fedor Borshev <fedor@borshev.com>
2021-01-09 15:59:44 +0300

    Ability to subscribe user to the particular mailchimp list

--- src/app/tests/mailchimp/tests_mailchimp_subscribe_task.py
+++ src/app/tests/mailchimp/tests_mailchimp_subscribe_task.py
@@ -24,6 +24,17 @@ def test_task(user, mass_subscribe):
     )
 
 
+def test_particular_list_id(user, mass_subscribe):
+    tasks.subscribe_to_mailchimp.delay(user.pk, list_id='TESTING-LIST-ID')
+
+    mass_subscribe.assert_called_once_with(
+        list_id='TESTING-LIST-ID',
+        members=[
+            MailchimpMember(email='test@e.mail', first_name='Rulon', last_name='Oboev'),
+        ],
+    )
+
+
 def test_task_does_not_do_things_if_there_is_no_mailchimp_contact_list_id(user, mass_subscribe, settings):
     settings.MAILCHIMP_CONTACT_LIST_ID = None
 
