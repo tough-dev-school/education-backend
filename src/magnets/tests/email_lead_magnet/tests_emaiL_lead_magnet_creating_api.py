14ac23f36726264ab3cc535404113db03975b2bf
Fedor Borshev <fedor@borshev.com>
2021-01-26 23:40:56 +0300

    Lead magnets tests revamp

--- src/magnets/tests/email_lead_magnet/tests_emaiL_lead_magnet_creating_api.py
+++ src/magnets/tests/email_lead_magnet/tests_emaiL_lead_magnet_creating_api.py
@@ -9,11 +9,16 @@ def get_user():
     return User.objects.last()
 
 
-def test_creating(api):
-    api.post('/api/v2/leads/email/eggs/', {
+@pytest.fixture
+def lead_data():
+    return {
         'name': 'Monty Python',
         'email': 'monty@python.org',
-    }, format='multipart')
+    }
+
+
+def test_creating(api, lead_data):
+    api.post('/api/v2/leads/email/eggs/', lead_data, format='multipart')
 
     created = get_user()
 
@@ -22,27 +27,24 @@ def test_creating(api):
     assert created.email == 'monty@python.org'
 
 
-def test_creating_response(api):
-    got = api.post('/api/v2/leads/email/eggs/', {
-        'name': 'Monty Python',
-        'email': 'monty@python.org',
-    }, format='multipart')
+def test_creating_response(api, lead_data):
+    got = api.post('/api/v2/leads/email/eggs/', lead_data, format='multipart')
 
     assert got['ok'] is True
     assert got['message'] == 'No spam, only ham'
 
 
-def test_nameless(api):
-    api.post('/api/v2/leads/email/eggs/', {
-        'email': 'monty@python.org',
-    }, format='multipart')
+def test_nameless(api, lead_data):
+    del lead_data['name']
+
+    api.post('/api/v2/leads/email/eggs/', lead_data, format='multipart')
 
     created = get_user()
 
     assert created.email == 'monty@python.org'
 
 
-def test_wrong_email(api):
-    api.post('/api/v2/leads/email/eggs/', {
-        'name': 'Monty Python',
-    }, format='multipart', expected_status_code=400)
+def test_emailless_should_fail(api, lead_data):
+    del lead_data['email']
+
+    api.post('/api/v2/leads/email/eggs/', lead_data, format='multipart', expected_status_code=400)
