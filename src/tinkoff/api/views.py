29c3ab1aa6d351f486f9175e55f133dfcbba0128
Fedor Borshev <fedor@borshev.com>
2021-02-07 16:38:21 +0300

    Tinkoff Credit webhooks (#198)

--- src/tinkoff/api/views.py
+++ src/tinkoff/api/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 
 from app.views import AnonymousAPIView
-from tinkoff.api.serializers import PaymentNotificationSerializer
+from tinkoff.api.permissions import TinkoffCreditNetmaskPermission
+from tinkoff.api.serializers import CreditNotificationSerializer, PaymentNotificationSerializer
 
 
 class TinkoffPaymentNotificationsView(AnonymousAPIView):
     def post(self, request, *args, **kwargs):
         serializer = PaymentNotificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return HttpResponse('OK')
+
+
+class TinkoffCreditNotificationsView(AnonymousAPIView):
+    permission_classes = [TinkoffCreditNetmaskPermission]
+
+    def post(self, request, *args, **kwargs):
+        serializer = CreditNotificationSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
 
