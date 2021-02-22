b66cb06d67ac0344cf3b45de748a1c75e044c9a5
Fedor Borshev <fedor@borshev.com>
2021-02-22 18:13:56 +0300

    Fixed DRF exceptions logging

--- src/app/sentry_exception_handler.py
+++ src/app/sentry_exception_handler.py
@@ -1,12 +1,9 @@
-from logging import exception
+import sentry_sdk
 from rest_framework.views import exception_handler
-from sentry_sdk import capture_exception
 
 
 def sentry_exception_handler(exc, context):
-    """Log all DRF exceptions to sentry"""
-    response = exception_handler(exc, context)
+    """Log all DRF exceptions to sentry, including ValidationErrors"""
+    sentry_sdk.capture_exception(exc)
 
-    capture_exception(exception)
-
-    return response
+    return exception_handler(exc, context)
