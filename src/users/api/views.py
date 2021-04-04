eb981cc620655505c5ad832f028cf0d436cb7db2
Fedor Borshev <fedor@borshev.com>
2021-04-04 14:22:00 +0300

    Dropped legacy user API

--- src/users/api/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import RetrieveAPIView
-
-from users.api.serializers import UserSerializer
-from users.models import User
-
-
-class UserView(RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
