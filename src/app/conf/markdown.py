ba3244722bf1a0a3f1eed9835235e8592e86d441
Fedor Borshev <fedor@borshev.com>
2021-04-27 23:09:02 +0300

    Migrated to cmarkgfm for markdown rendering (#275)

--- src/app/conf/markdown.py
+++ src/app/conf/markdown.py
@@ -1,3 +1,28 @@
+from bleach.sanitizer import ALLOWED_ATTRIBUTES as _ALLOWED_ATTRIBUTES
+from bleach.sanitizer import ALLOWED_TAGS as _ALLOWED_TAGS
+
 MARKDOWNX_URLS_PATH = '/api/v2/markdownx/markdownify/'
 MARKDOWNX_UPLOAD_URLS_PATH = '/api/v2/markdownx/upload/'
 MARKDOWNX_MARKDOWNIFY_FUNCTION = 'app.markdown.markdownify'
+
+BLEACH_ALLOWED_TAGS = [
+    *_ALLOWED_TAGS,
+    'p',
+    'img',
+    'br',
+    'hr',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h3',
+]
+
+BLEACH_ALLOWED_ATTRIBUTES = {
+    **_ALLOWED_ATTRIBUTES,
+    'img': {
+        'src',
+        'alt',
+    },
+}
