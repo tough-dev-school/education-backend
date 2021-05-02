dc2d3593865794b4b6d6b187e95bdc9d817a23ea
Fedor Borshev <fedor@borshev.com>
2021-05-02 10:56:24 +0300

    Fixed a warning during boot up with empty .env file

--- src/app/conf/environ.py
+++ src/app/conf/environ.py
@@ -1,12 +1,13 @@
 """Read .env file"""
 import environ
+import os.path
 
 env = environ.Env(
     DEBUG=(bool, False),
     CI=(bool, False),
 )
-
-environ.Env.read_env('app/.env')                  # reading .env file
+if os.path.exists('app/.env'):
+    environ.Env.read_env('app/.env')                  # reading .env file
 
 __all__ = [
     env,
