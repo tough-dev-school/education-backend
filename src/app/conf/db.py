3f2e56815e67b20dd29f862212e58b90e6e9be4b
Fedor Borshev <fedor@borshev.com>
2021-05-09 13:14:54 +0300

    Explicit DEFAULT_AUTO_FIELD (Getting ready for django 3.2)

--- src/app/conf/db.py
+++ src/app/conf/db.py
@@ -6,5 +6,7 @@ DATABASES = {
     'default': env.db(),    # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'  # https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+
 if not env('DEBUG', cast=bool):
     DATABASES['default']['CONN_MAX_AGE'] = 600
