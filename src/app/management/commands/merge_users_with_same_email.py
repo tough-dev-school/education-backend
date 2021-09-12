d80a65c6d9dd051844fcd2240f72fcd39ee9d26b
kiddick <kiddick@users.noreply.github.com>
2021-09-12 18:32:36 +0300

    add command to merge users

---
+++ src/app/management/commands/merge_users_with_same_email.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+from django.apps import apps
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Clean antispam logs for outgoing email and marketing triggers'
+
+    def handle(self, *args, **kwargs):
+        data = defaultdict(list)
+        user_model = apps.get_model('users.User')
+        users = user_model.objects.all()
+        for user in users:
+            data[user.email.lower()].append(user.pk)
+        for email, ids in data.items():
+            if len(ids) > 1:
+                users = user_model.objects.filter(id__in=ids).order_by('date_joined').all()
+                self.stdout.write(f'{users.count()} users with email {email} will be merged..')
+                result_user = users[0]
+                subscribed = any([u.subscribed for u in users])
+                result_user.subscribed = subscribed
+                result_user.save()
+                for u in users[1:]:
+                    u.delete()
+                self.stdout.write(self.style.SUCCESS('Done!'))
