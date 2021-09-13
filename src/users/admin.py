3008f6f3d83e842927323042713bc1fc836b9959
Fedor Borshev <fedor@borshev.com>
2021-09-13 13:22:25 +0300

    Gender admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
 
 from app.admin import admin
 from users.creator import UserCreator
@@ -36,6 +37,15 @@ class UserAdmin(BaseUserAdmin):
                 'fields': ['email'],
             },
         ),
+    )
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'gender')}),
+        (_('Name in english'), {'fields': ('first_name_en', 'last_name_en')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
 
