4691c81f379077ec412a5e41703a239161fa7060
Fedor Borshev <fedor@borshev.com>
2021-10-22 19:41:34 +0300

    Enabled celery acks_late option to not loose data when background tasks has failed

--- src/app/celery.py
+++ src/app/celery.py
@@ -18,6 +18,7 @@ celery.conf.update(
     task_always_eager=env('CELERY_ALWAYS_EAGER', cast=bool, default=settings.DEBUG),  # by default in debug mode we run all celery tasks in foregroud.
     task_eager_propagates=True,
     task_ignore_result=True,
+    task_acks_late=True,
     timezone=env('TIME_ZONE', cast=str, default='Europe/Moscow'),
     enable_utc=False,
     beat_schedule={
