5dc9d308d19ea54518372e7c230f0a95e0c17ca5
Fedor Borshev <fedor@borshev.com>
2021-10-26 19:26:49 +0300

    Dropped select2 code, cuz it was broken for a long time

--- src/app/admin/widgets.py
+++ src/app/admin/widgets.py
@@ -1,4 +1,4 @@
-from django.forms.widgets import Select, TextInput
+from django.forms.widgets import TextInput
 
 
 class TextInputWithoutAutocomplete(TextInput):
@@ -9,21 +9,3 @@ class TextInputWithoutAutocomplete(TextInput):
         attrs['autocomplete'] = 'off'
 
         super().__init__(attrs)
-
-
-class Select2Widget(Select):
-    """
-    Fancy select widget based on Select2 (https://select2.github.io/)
-    """
-    def __init__(self, attrs=None, **kwargs):
-        """
-        Adds a special class to the widget
-        """
-        attrs = attrs or {}
-        classes = attrs.get('class', '')
-
-        if 'select2 ' not in classes:
-            classes = 'select2 ' + classes
-            attrs['class'] = classes
-
-        super().__init__(attrs, **kwargs)
