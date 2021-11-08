1a1e99e7f451544ea13e888e485e1f40955f7e16
Maxim romanyuk <metheoryt@gmail.com>
2021-11-08 23:48:22 +0600

    #265 improve code readability, rewrite tests

--- src/users/management/commands/merge_same_email_users.py
+++ src/users/management/commands/merge_same_email_users.py
@@ -1,90 +1,59 @@
-from typing import TYPE_CHECKING
-
-from django.apps import apps
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
-from django.db.models import functions as fn, Count
+from django.db.models import functions as fn
 from django.db.transaction import atomic
 
-if TYPE_CHECKING:
-    from users.models import User
-
-USER_RELATIONS = {
-    'passwordlessauthtoken_set': 'user',
-    'answer_set': 'author',
-    'leadcampaignlogentry_set': 'user',
-    'order_set': 'user',
-    'created_gifts': 'giver',
-    'answeraccesslogentry_set': 'user',  # UC answer, user
-    'answercrosscheck_set': 'checker',  # UC answer, checker
-    'study_set': 'student'  # UC student, course
-}
-USER_PROPERTIES = (
-    'first_name',
-    'last_name',
-    'first_name_en',
-    'last_name_en',
-    'subscribed',
-    'gender'
-)
+from a12n.models import PasswordlessAuthToken
+from homework.models import Answer, AnswerAccessLogEntry, AnswerCrossCheck
+from magnets.models import LeadCampaignLogEntry
+from orders.models import Order
+from studying.models import Study
+from users.models import User
 
 
 class Command(BaseCommand):
     help = 'merge active user accounts with same email into one latest joined'
 
-    def users_query(self):
-        """
-        active users, who has email, annotated with lowercased email version
-        """
-        UserClass = apps.get_model('users.User')  # noqa
-        return UserClass.objects\
-            .filter(is_active=True)\
-            .filter(email__contains='@')\
-            .annotate(email_lower=fn.Lower('email'))
-
-    def duplicate_email_query(self):
-        """
-        amount of users by email (case insensitive),
-        if there are more than 1
-        """
-        q = self.users_query()
-        return q.values('email_lower') \
-            .annotate(count=Count('email_lower')) \
-            .filter(count__gt=1)
-
-    def merge_user(self, source: 'User', target: 'User'):
+    def merge_user(self, source: User, target: User):
         """Merge source user into target user, considering all relations.
         Some relations, which have unique constraint on user,
         might be duplicated as result of merge. Such relations are left untouched with source user.
         """
-        for prop_name in USER_PROPERTIES:
-            # update target property only if target is empty/false,
-            # and source is not empty/true
-            target_prop = getattr(target, prop_name)
-            source_prop = getattr(source, prop_name)
-            if not target_prop and source_prop:
-                self.stdout.write(f'updating "{target}".{prop_name} from "{source}"')
-                setattr(target, prop_name, source_prop)
+        self.stdout.write(f'merging "{source}" into "{target}"')
 
-        # Merging user relations
-        for relation_attr, relation_name in USER_RELATIONS.items():
-            relation = getattr(source, relation_attr)
-            for i in relation.all():
-                self.stdout.write(f'moving {i} from "{source}".{relation_attr} to "{target}"')
-                setattr(i, relation_name, target)
+        # merging user properties
+        target.first_name = target.first_name or source.first_name
+        target.last_name = target.last_name or source.last_name
+        target.first_name_en = target.first_name_en or source.first_name_en
+        target.last_name_en = target.last_name_en or source.last_name_en
+        target.subscribed = target.subscribed or source.subscribed
+        target.gender = target.gender or source.gender
 
-                with atomic():
-                    # we already run whole `handle` method inside transaction,
-                    # so we need another transaction layer to handle integrity errors with grace
-                    # https://docs.djangoproject.com/en/3.2/topics/db/transactions/
-                    try:
-                        i.save()
-                    except IntegrityError as e:
-                        # There are some user relations, that have unique constraint, involving user reference.
-                        # So a situation is possible, when relation that being moved,
-                        # already has a duplicate in target relation set
-                        # Current tactic is to ignore such cases and to leave the relation in source
-                        self.stdout.write(f'integrity error on saving "{i}": {e}')
+        # Merging safe user relations
+        PasswordlessAuthToken.objects.filter(user=source).update(user=target)
+        Answer.objects.filter(author=source).update(author=target)
+        LeadCampaignLogEntry.objects.filter(user=source).update(user=target)
+        Order.objects.filter(user=source).update(user=target)
+        Order.objects.filter(giver=source).update(giver=target)
+
+        # Merging possibly overlapping relations, leaving as is on collision
+        try:
+            with atomic():
+                AnswerAccessLogEntry.objects.filter(user=source).update(user=target)  # UC answer user
+        except IntegrityError as e:
+            self.stdout.write(f'integrity error on updating "{source}" AnswerAccessLogEntry: {e}')
+
+        try:
+            with atomic():
+                AnswerCrossCheck.objects.filter(checker=source).update(checker=target)  # UC answer checker
+        except IntegrityError as e:
+            self.stdout.write(f'integrity error on updating "{source}" AnswerCrossCheck: {e}')
+
+        try:
+            with atomic():
+                Study.objects.filter(student=source).update(student=target)  # UC student course
+        except IntegrityError as e:
+            self.stdout.write(f'integrity error on updating "{source}" Study: {e}')
 
         # We will lowercase username as well as email,
         # but for username, we need to keep it unique.
@@ -92,29 +61,32 @@ class Command(BaseCommand):
         source.username = source.uuid
         source.is_active = False  # disable user instead of physically delete it
         source.save()
-        self.stdout.write(f'finished merging "{source}" into "{target}"')
+        self.stdout.write(f'merged "{source}" into "{target}"')
 
     def handle_single_email(self, email):
-        """Merges all users with same email (case insensitive),
+        """Merges all users with sa me email (case insensitive),
         into last registered user, in order of join date.
         """
-        self.stdout.write(f'working on "{email.lower()}"')
-        same_users = self.users_query().filter(email__iexact=email).order_by('-date_joined').all()
-        target_user, source_user_list = same_users[0], same_users[1:]
+        self.stdout.write(f'handling {email}')
+        same_users = User.objects.filter(is_active=True).filter(email__iexact=email).order_by('-date_joined')
+        target, sources = same_users[0], same_users[1:]
 
-        for source_user in source_user_list:
-            self.merge_user(source_user, target_user)
+        for source in sources:
+            self.merge_user(source, target)
 
-        target_user.username = target_user.username.lower()
-        target_user.email = target_user.email.lower()
-        target_user.save()
-        self.stdout.write(f'finish merging {email}')
+        target.username = target.username.lower()
+        target.email = target.email.lower()
+        target.save()
+        self.stdout.write(f'handled {email}')
 
     @atomic
     def handle(self, *args, **options):
-        duplicate_emails = self.duplicate_email_query()
+        # all emails
+        q = User.objects\
+            .filter(is_active=True)\
+            .annotate(lower_email=fn.Lower('email'))\
+            .values('lower_email')\
+            .distinct()
 
-        for record in duplicate_emails:
-            self.handle_single_email(record['email_lower'])
-
-        self.stdout.write(f'{len(duplicate_emails)} emails are handled!')
+        for row in q:
+            self.handle_single_email(row['lower_email'])
--- src/users/tests/management/test_same_email_user_merge.py
+++ src/users/tests/management/test_same_email_user_merge.py
@@ -1,176 +1,223 @@
 from datetime import datetime
-from typing import Tuple
-from uuid import uuid4
 
 import pytest
 from django.core import management
 from mixer.backend.django import mixer
+from pytest_mock import MockerFixture
 
-from homework.models import Answer
-from products.models import Course
-from users.management.commands.merge_same_email_users import Command
+from users.management.commands import merge_same_email_users
 from users.models import User
 
 pytestmark = pytest.mark.django_db
 
 
-johnnys_uuid = uuid4()
+@pytest.fixture
+def bob_a() -> User:
+    return mixer.blend('users.User', username='boB@EXAMPLE.com', email='boB@EXAMPLE.com', date_joined=datetime(2021, 11, 1))
 
 
-def link_data_to_user(user):
-    # an ordinary user and his data
-    johnny = User.objects.filter(username='johnny').first() \
-        or mixer.blend('users.User', username='johnny')
-    johnnys_answer = Answer.objects.filter(slug=johnnys_uuid).first() \
-        or mixer.blend('homework.Answer', slug=johnnys_uuid, author=johnny)
-
-    mixer.blend('a12n.PasswordlessAuthToken', user=user)
-    mixer.blend('homework.Answer', author=user)
-
-    # access to same answer - will not be merged
-    mixer.blend('homework.AnswerAccessLogEntry', answer=johnnys_answer, user=user)  # UC answer user
-    # check of the same answer - will not be merged
-    mixer.blend('homework.AnswerCrossCheck', answer=johnnys_answer, checker=user)  # UC answer checker
-    mixer.blend('magnets.LeadCampaignLogEntry', user=user)
-    mixer.blend('orders.Order', user=user)
-    mixer.blend('orders.Order', user=johnny, giver=user)
-
-    # personal course, will be merged
-    mixer.blend('studying.Study', student=user)  # UC student course
-
-    # shared course, will not be merged
-    shared_course = Course.objects.filter(name='the course').first() \
-        or mixer.blend('products.Course', name='the course')
-    mixer.blend('studying.Study', student=user, course=shared_course)  # UC student course
+@pytest.fixture
+def bob_b() -> User:
+    return mixer.blend('users.User', username='Bob@Example.Com', email='Bob@Example.Com', date_joined=datetime(2021, 11, 2))
 
 
-def create_user(**kwargs):
-    u = User.objects.create(**kwargs)
-    link_data_to_user(u)
-    return u
+@pytest.fixture
+def bob_c() -> User:
+    return mixer.blend('users.User', username='BOB@EXAMPLE.COM', email='BOB@EXAMPLE.COM', date_joined=datetime(2021, 11, 3))
 
 
-def create_users_to_merge() -> Tuple[User, User, User]:
-    first = create_user(
-        username='Test@test.com',
-        first_name='Имя-1',
-        last_name='Фамилия-1',
-        email='Test@test.com',
-        date_joined=datetime(2000, 1, 1),
-        subscribed=True,
-        first_name_en='Name-1',
-        last_name_en='Lastname-1',
-        gender='male'
-    )
-    second = create_user(
-        username='test@test.com',
-        first_name='',
-        last_name='Фамилия-2',
-        email='test@test.com',
-        date_joined=datetime(2000, 1, 2),
-        subscribed=False,
-        first_name_en='Name-2',
-        last_name_en='',
-        gender='female'
-    )
-    third = create_user(
-        username='teST@test.com',
-        first_name='Имя-3',
-        last_name='',
-        email='teST@test.com',
-        date_joined=datetime(2000, 1, 3),
-        subscribed=False,
-        first_name_en='',
-        last_name_en='',
-        gender=''
-    )
-    return first, second, third
+@pytest.fixture
+def command():
+    return merge_same_email_users.Command()
 
 
-def test_command_works():
-    create_users_to_merge()
+@pytest.fixture
+def handle_single_email_mock(mocker: MockerFixture):
+    return mocker.patch('users.management.commands.merge_same_email_users.Command.handle_single_email')
 
-    assert Command().duplicate_email_query().count() == 1  # 1 group of 3 duplicates
 
+#
+# TEST USER MERGE
+#
+
+
+def test_merged_user_becomes_inactive(bob_a, bob_b, command):
+    command.merge_user(source=bob_a, target=bob_b)
+    assert not bob_a.is_active
+
+
+def test_merged_user_changes_username_to_uuid(bob_a, bob_b, command):
+    command.merge_user(source=bob_a, target=bob_b)
+    assert bob_a.username == bob_a.uuid
+
+
+@pytest.mark.parametrize('prop_name,prop_value,empty_value', [
+    ('first_name', 'Боб', ''),
+    ('last_name', 'Бобов', ''),
+    ('first_name_en', 'Bob', ''),
+    ('last_name_en', 'McBob', ''),
+    ('subscribed', True, False),
+    ('gender', 'male', '')
+])
+def test_target_user_populates_empty_property(bob_a, bob_b, command, prop_name, prop_value, empty_value):
+    setattr(bob_a, prop_name, prop_value)
+    bob_a.save()
+    setattr(bob_b, prop_name, empty_value)
+    bob_b.save()
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    assert getattr(bob_b, prop_name) == prop_value
+
+
+@pytest.mark.parametrize('prop_name,prop_value,empty_value', [
+    ('first_name', 'Боб', ''),
+    ('last_name', 'Бобов', ''),
+    ('first_name_en', 'Bob', ''),
+    ('last_name_en', 'McBob', ''),
+    ('subscribed', True, False),
+    ('gender', 'male', '')
+])
+def test_target_user_preserves_non_empty_property(bob_a, bob_b, command, prop_name, prop_value, empty_value):
+    setattr(bob_a, prop_name, empty_value)
+    bob_a.save()
+    setattr(bob_b, prop_name, prop_value)
+    bob_b.save()
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    assert getattr(bob_b, prop_name) == prop_value
+
+
+@pytest.mark.parametrize('model,rel_name', [
+    ('a12n.PasswordlessAuthToken', 'user'),
+    ('homework.Answer', 'author'),
+    ('homework.AnswerAccessLogEntry', 'user'),
+    ('homework.AnswerCrossCheck', 'checker'),
+    ('magnets.LeadCampaignLogEntry', 'user'),
+    ('orders.Order', 'user'),
+    ('orders.Order', 'giver'),
+    ('studying.Study', 'student')
+])
+def test_source_user_relation_merged_into_target_user(bob_a, bob_b, command, model, rel_name):
+    bob_a_rel = mixer.blend(model, **{rel_name: bob_a})
+    command.merge_user(bob_a, bob_b)
+    bob_a_rel.refresh_from_db()
+    assert getattr(bob_a_rel, rel_name) == bob_b
+
+
+def test_source_user_preserves_duplicated_relation_answer_access_log_entry(bob_a, bob_b, command):
+    answer = mixer.blend('homework.Answer')
+    rel_a = mixer.blend('homework.AnswerAccessLogEntry', user=bob_a, answer=answer)
+    rel_b = mixer.blend('homework.AnswerAccessLogEntry', user=bob_b, answer=answer)
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    rel_a.refresh_from_db()
+    assert rel_a.user == bob_a
+
+    rel_b.refresh_from_db()
+    assert rel_b.user == bob_b
+
+
+def test_source_user_preserves_duplicated_relation_answer_cross_check(bob_a, bob_b, command):
+    answer = mixer.blend('homework.Answer')
+    rel_a = mixer.blend('homework.AnswerCrossCheck', checker=bob_a, answer=answer)
+    rel_b = mixer.blend('homework.AnswerCrossCheck', checker=bob_b, answer=answer)
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    rel_a.refresh_from_db()
+    assert rel_a.checker == bob_a
+
+    rel_b.refresh_from_db()
+    assert rel_b.checker == bob_b
+
+
+def test_source_user_preserves_duplicated_relation_study(bob_a, bob_b, command):
+    course = mixer.blend('products.Course')
+    rel_a = mixer.blend('studying.Study', student=bob_a, course=course)
+    rel_b = mixer.blend('studying.Study', student=bob_b, course=course)
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    rel_a.refresh_from_db()
+    assert rel_a.student == bob_a
+
+    rel_b.refresh_from_db()
+    assert rel_b.student == bob_b
+
+
+@pytest.mark.parametrize('rel_model,rel_name,constraints', [
+    ('studying.Study', 'student', {'course': 'products.Course'}),
+    ('homework.AnswerCrossCheck', 'checker', {'answer': 'homework.Answer'}),
+    ('homework.AnswerAccessLogEntry', 'user', {'answer': 'homework.Answer'}),
+])
+def test_source_user_preserves_duplicated_relations(bob_a, bob_b, command, rel_model, rel_name, constraints):
+    """Parametrized version of 3 tests above, written for comparison"""
+    for k, v in constraints.items():
+        constraints[k] = mixer.blend(v)
+
+    rel_a = mixer.blend(rel_model, **{rel_name: bob_a}, **constraints)
+    rel_b = mixer.blend(rel_model, **{rel_name: bob_b}, **constraints)
+
+    command.merge_user(source=bob_a, target=bob_b)
+
+    rel_a.refresh_from_db()
+    assert getattr(rel_a, rel_name) == bob_a
+
+    rel_b.refresh_from_db()
+    assert getattr(rel_b, rel_name) == bob_b
+
+
+#
+# TEST SINGLE EMAIL MERGE
+#
+
+def test_target_user_lower_email(bob_a, bob_b, command, mocker: MockerFixture):
+    command.merge_user = mocker.MagicMock()
+    command.handle_single_email(bob_a.email)
+    bob_b.refresh_from_db()
+    assert bob_b.email == bob_b.email.lower()
+
+
+def test_target_user_lower_username(bob_a, bob_b, command, mocker: MockerFixture):
+    command.merge_user = mocker.MagicMock()
+    command.handle_single_email(bob_a.email)
+    bob_b.refresh_from_db()
+    assert bob_b.username == bob_b.username.lower()
+
+
+def test_two_users_merge_into_latest(bob_a, bob_b, command, mocker: MockerFixture):
+    command.merge_user = mocker.MagicMock()
+    command.handle_single_email(bob_a.email)
+    command.merge_user.assert_called_once_with(bob_a, bob_b)
+
+
+def test_three_users_merge_into_latest(bob_a, bob_b, bob_c, command, mocker: MockerFixture):
+    command.merge_user = mocker.MagicMock()
+    command.handle_single_email(bob_a.email)
+    command.merge_user.assert_has_calls((
+        mocker.call(bob_b, bob_a),
+        mocker.call(bob_c, bob_a)
+    ))
+
+
+#
+# TEST WHOLE COMMAND
+#
+
+def test_command_handles_email_case_insensitive(bob_a, bob_b, bob_c, handle_single_email_mock):
     management.call_command('merge_same_email_users')
-
-    assert Command().duplicate_email_query().count() == 0  # no groups
-
-
-def test_source_user_becomes_inactive():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=second, target=third)
-    assert not second.is_active
+    handle_single_email_mock.assert_called_once_with('bob@example.com')
 
 
-def test_source_user_changes_username_to_uuid():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=second, target=third)
-    assert second.username == second.uuid
+def test_command_handles_single_account_email(bob_a, handle_single_email_mock):
+    management.call_command('merge_same_email_users')
+    handle_single_email_mock.assert_called_once_with('bob@example.com')
 
 
-def test_source_user_loses_relations():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=second, target=third)
-
-    assert second.passwordlessauthtoken_set.count() == 0
-    assert second.answer_set.count() == 0
-    assert second.leadcampaignlogentry_set.count() == 0
-    assert second.order_set.count() == 0
-    assert second.created_gifts.count() == 0
-
-
-def test_target_user_takes_source_user_relations():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=second, target=third)
-
-    assert third.passwordlessauthtoken_set.count() == 2
-    assert third.answer_set.count() == 2
-    assert third.leadcampaignlogentry_set.count() == 2
-    assert third.order_set.count() == 2
-    assert third.created_gifts.count() == 2
-    assert third.study_set.count() == 3
-
-
-def test_source_user_preserves_duplicated_relations():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=second, target=third)
-
-    assert second.answeraccesslogentry_set.count() == 1
-    assert second.answercrosscheck_set.count() == 1
-    assert second.study_set.count() == 1
-
-
-def test_target_user_populates_empty_properties():
-    first, second, third = create_users_to_merge()
-
-    cmd = Command()
-    cmd.merge_user(source=first, target=third)
-
-    assert third.last_name == first.last_name
-    assert third.first_name_en == first.first_name_en
-    assert third.last_name_en == first.last_name_en
-    assert third.gender == first.gender
-    assert third.subscribed is first.subscribed
-    # ? assert third.password
-    # ? assert third.last_login
-
-
-def test_target_user_preserves_non_empty_properties():
-    first, second, third = create_users_to_merge()
-    first_name = third.first_name
-
-    cmd = Command()
-    cmd.merge_user(source=first, target=third)
-
-    assert third.first_name == first_name
+def test_command_doesnt_handle_disabled_accounts(bob_a, handle_single_email_mock):
+    mixer.blend('users.User', email='alice@gmail.com', is_active=False)
+    management.call_command('merge_same_email_users')
+    handle_single_email_mock.assert_called_once_with('bob@example.com')
