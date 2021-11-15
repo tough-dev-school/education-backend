c52f6bbdd305a093011452c3357bf742cf416940
Maxim romanyuk <metheoryt@gmail.com>
2021-11-15 23:18:37 +0600

    #265 fix tests

--- src/users/tests/management/merge_same_email_users/test_command.py
+++ src/users/tests/management/merge_same_email_users/test_command.py
@@ -10,16 +10,20 @@ def handle_single_email_mock(mocker: MockerFixture):
     return mocker.patch('users.management.commands.merge_same_email_users.Command.handle_single_email')
 
 
-def test_command_handles_email_case_insensitive(bob_a, bob_b, bob_c, handle_single_email_mock):
-    management.call_command('merge_same_email_users')
-
-    handle_single_email_mock.assert_called_once_with('bob@example.com')
-
-
 def test_command_handles_single_account_email(bob_a, handle_single_email_mock):
     management.call_command('merge_same_email_users')
 
-    handle_single_email_mock.assert_called_once_with('bob@example.com')
+    handle_single_email_mock.assert_called_once_with('boB@EXAMPLE.com')
+
+
+def test_command_handles_every_user_email(bob_a, bob_b, bob_c, handle_single_email_mock, mocker):
+    management.call_command('merge_same_email_users')
+
+    handle_single_email_mock.assert_has_calls((
+        mocker.call('boB@EXAMPLE.com'),
+        mocker.call('Bob@Example.Com'),
+        mocker.call('BOB@EXAMPLE.COM')
+    ), any_order=True)
 
 
 def test_command_doesnt_handle_disabled_accounts(mixer, bob_a, handle_single_email_mock):
@@ -27,4 +31,4 @@ def test_command_doesnt_handle_disabled_accounts(mixer, bob_a, handle_single_ema
 
     management.call_command('merge_same_email_users')
 
-    handle_single_email_mock.assert_called_once_with('bob@example.com')
+    handle_single_email_mock.assert_called_once_with('boB@EXAMPLE.com')
