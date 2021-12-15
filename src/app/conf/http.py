d4982a545c68ea74e4ab18b8776d1c6a9e4376fa
Fedor Borshev <fedor@borshev.com>
2021-12-15 20:38:39 +0300

    Final domain brading

--- src/app/conf/http.py
+++ src/app/conf/http.py
@@ -1,25 +1,24 @@
 from app.conf.environ import env
 
-ABSOLUTE_HOST = env('ABSOLUTE_HOST', cast=str, default='https://edu-app.borshev.com')
+ABSOLUTE_HOST = env('ABSOLUTE_HOST', cast=str, default='https://app.tough-dev.school')
 ALLOWED_HOSTS = [
-    'education-backend.herokuapp.com',
-    'edu-app.borshev.com',
     'localhost',
     'localhost:8000',
-    'education.borshev.com',
     ABSOLUTE_HOST.replace('https://', ''),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'education.borshev.com',
     'borshev.com',
+    'tough-dev.school',
 ]
 CORS_ALLOWED_ORIGINS = [
     'https://education.borshev.com',
+    'https://certificates.tough-dev.school',
 ]
 CORS_ORIGIN_REGEX_WHITELIST = [
     r'.*education-frontend.netlify.app.*',
 ]
 
-FRONTEND_URL = env('FRONTEND_URL', cast=str, default='https://education.borshev.com/lms/')
-DIPLOMA_FRONTEND_URL = env('DIPLOMA_FRONTEND_URL', cast=str, default='https://certificates.borshev.com/')
+FRONTEND_URL = env('FRONTEND_URL', cast=str, default='https://lms.tough-dev.school/lms/')
+DIPLOMA_FRONTEND_URL = env('DIPLOMA_FRONTEND_URL', cast=str, default='https://certificates.tough-dev.school/')
