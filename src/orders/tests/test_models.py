3e86a81a01b0a6633d0c7c8452251f2785652dc4
Abdujabbar Mirkhalikov <abdujabbar.mirkhalikov@gmail.com>
2022-01-01 22:44:50 +0500

    added test cases for method only_one_or_zero_is_set

--- src/orders/tests/test_models.py
+++ src/orders/tests/test_models.py
@@ -1,7 +1,8 @@
 import pytest
+from django.db.models import Q
 from django.db.utils import IntegrityError
 
-from orders.models.order import Order
+from orders.models.order import Order, only_one_or_zero_is_set
 
 pytestmark = [pytest.mark.django_db]
 
@@ -24,6 +25,60 @@ def bundle(mixer):
 @pytest.fixture
 def student(mixer):
     return mixer.blend('users.User', first_name='Омон', last_name='Кривомазов', gender='male')
+
+
+def test_only_one_or_zero_is_set():
+    cases = [
+        Q(
+            Q(
+                Q(
+                    ('bundle__isnull', False),
+                    ('course__isnull', True),
+                    ('record__isnull', True),
+                ),
+                Q(
+                    ('bundle__isnull', True),
+                    ('course__isnull', False),
+                    ('record__isnull', True),
+                ),
+                Q(
+                    ('bundle__isnull', True),
+                    ('course__isnull', True),
+                    ('record__isnull', False),
+                ),
+                Q(
+                    ('bundle__isnull', True),
+                    ('course__isnull', True),
+                    ('record__isnull', True),
+                ),
+                _connector='OR',
+            ),
+        ),
+        Q(
+            Q(
+                Q(
+                    ('bundle__isnull', False),
+                    ('course__isnull', True),
+                ),
+                Q(
+                    ('bundle__isnull', True),
+                    ('course__isnull', False),
+                ),
+                Q(
+                    ('bundle__isnull', True),
+                    ('course__isnull', True),
+                ),
+                _connector='OR',
+            ),
+        ),
+    ]
+    expected = [
+        only_one_or_zero_is_set('bundle', 'course', 'record'),
+        only_one_or_zero_is_set('bundle', 'course'),
+    ]
+
+    for i, case in enumerate(cases):
+        assert case == expected[i], f'Trouble for the input: {case} != {expected[i]}'
 
 
 def test_order_constraints_check_product_with_two_items(student, record, course):
