3c2d1f0f87f1d30552a81866565563c333bd0d74
Fedor Borshev <fedor@borshev.com>
2022-01-08 13:52:12 +0300

    YAGNI: dropped comments and discussions cuz

--- src/notion/page.py
+++ src/notion/page.py
@@ -8,13 +8,9 @@ from notion.block import NotionBlockList
 @dataclass
 class NotionPage:
     blocks: NotionBlockList
-    comments: Optional[NotionBlockList] = NotionBlockList()
-    discussions: Optional[NotionBlockList] = NotionBlockList()
 
     @classmethod
     def from_api_response(cls, api_response: dict) -> 'NotionPage':
         return cls(
             blocks=NotionBlockList.from_api_response(api_response['recordMap']['block']),
-            comments=NotionBlockList.from_api_response(api_response['recordMap']['comment']),
-            discussions=NotionBlockList.from_api_response(api_response['recordMap']['discussion']),
         )
--- src/notion/tests/test_fetch_page.py
+++ src/notion/tests/test_fetch_page.py
@@ -13,14 +13,6 @@ def _ok(httpx_mock: HTTPXMock):
                     'first-block': {},
                     'second-block': {},
                 },
-                'comment': {
-                    'first-comment': {},
-                    'second-comment': {},
-                },
-                'discussion': {
-                    'first-discussion': {},
-                    'second-discussion': {},
-                },
             },
         },
     )
@@ -30,9 +22,4 @@ def test_ok(notion):
     page = notion.fetch_page('0cb348b3a2d24c05bc944e2302fa553')
 
     assert len(page.blocks) == 2
-    assert len(page.discussions) == 2
-    assert len(page.comments) == 2
-
     assert page.blocks[0].id == 'first-block'
-    assert page.comments[0].id == 'first-comment'
-    assert page.discussions[0].id == 'first-discussion'
