9f6fa492383a44a2f9f8711d93f9dcfc3ec2a45f
Fedor Borshev <fedor@borshev.com>
2022-01-09 18:23:34 +0300

    Fetching notion page title

--- src/notion/page.py
+++ src/notion/page.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import contextlib
 from dataclasses import dataclass
 
 from notion.block import NotionBlockList
@@ -12,3 +15,9 @@ class NotionPage:
         return cls(
             blocks=NotionBlockList.from_api_response(api_response['recordMap']['block']),
         )
+
+    @property
+    def title(self) -> Optional[str]:
+        if self.blocks.first_page_block is not None:
+            with contextlib.suppress(KeyError, IndexError):
+                return self.blocks.first_page_block.data['value']['properties']['title'][0][0]
--- src/notion/tests/test_fetch_page.py
+++ src/notion/tests/test_fetch_page.py
@@ -10,7 +10,20 @@ def _ok(httpx_mock: HTTPXMock):
             'recordMap': {
                 '__version__': 3,
                 'block': {
-                    'first-block': {},
+                    'first-block': {
+                        'role': 'editor',
+                        'value': {
+                            'id': 'first-block',
+                            'type': 'page',
+                            'properties': {
+                                'title': [['Неделя 1 из 4']],
+                            },
+                            'content': [
+                                'third-block',
+                                'fourth-block',
+                            ],
+                        },
+                    },
                     'second-block': {},
                 },
             },
@@ -23,3 +36,33 @@ def test_ok(notion):
 
     assert len(page.blocks) == 2
     assert page.blocks[0].id == 'first-block'
+
+
+def test_page_title(notion):
+    page = notion.fetch_page('0cb348b3a2d24c05bc944e2302fa553')
+
+    assert page.title == 'Неделя 1 из 4'
+
+
+def test_abscense_of_the_page_block_does_not_break_page_title(notion):
+    page = notion.fetch_page('0cb348b3a2d24c05bc944e2302fa553')
+
+    del page.blocks[0]
+
+    assert page.title is None
+
+
+def test_block_without_title_does_not_break_page_title_1(notion):
+    page = notion.fetch_page('0cb348b3a2d24c05bc944e2302fa553')
+
+    del page.blocks[0].data['value']['properties']['title']
+
+    assert page.title is None
+
+
+def test_block_without_title_does_not_break_page_title_2(notion):
+    page = notion.fetch_page('0cb348b3a2d24c05bc944e2302fa553')
+
+    page.blocks[0].data['value']['properties']['title'] = []
+
+    assert page.title is None
