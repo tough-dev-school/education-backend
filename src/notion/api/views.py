f40f05260bc3f6a11a68d4d6238149469206802a
Fedor Borshev <fedor@borshev.com>
2022-01-09 12:59:04 +0300

    API view to fetch purchased notion materials (#679)

--- src/notion/api/views.py
+++ src/notion/api/views.py
@@ -1,24 +1,41 @@
-from rest_framework import permissions
+from django.db.models import QuerySet
+from django.shortcuts import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
+from app.views import AuthenticatedAPIView
 from notion.api.serializers import NotionPageSerializer
 from notion.client import NotionClient
+from notion.models import Material
+from studying.models import Study
 
 
-class NotionPageView(APIView):
-    serializer_class = NotionPageSerializer
-    permission_classes = [permissions.IsAdminUser]
-
+class NotionMaterialView(AuthenticatedAPIView):
     def get(self, request: Request, *args, **kwargs) -> Response:
-        page = self.notion.fetch_page_recursively(self.kwargs['page_id'])
+        material = self.get_material()
+
+        page = self.notion.fetch_page_recursively(material.page_id)
 
         return Response(
             data=NotionPageSerializer(page).data,
             status=200,
         )
 
+    def get_material(self) -> Material:
+        queryset = self.get_queryset()
+        return get_object_or_404(queryset, page_id=self.page_id)
+
+    def get_queryset(self) -> QuerySet[Material]:
+        if self.request.user.is_superuser or self.request.user.has_perm('notion.see_all_materials'):
+            return Material.objects.all()
+
+        available_courses = Study.objects.filter(student=self.request.user).values('course')
+        return Material.objects.filter(active=True, course__in=available_courses)
+
     @property
     def notion(self) -> NotionClient:
         return NotionClient()
+
+    @property
+    def page_id(self) -> str:
+        return NotionClient.id_to_uuid(self.kwargs['page_id'])
---
+++ src/notion/tests/api/conftest.py
@@ -0,0 +1,55 @@
+import pytest
+
+from notion.block import NotionBlock, NotionBlockList
+from notion.page import NotionPage
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture
+def api(api):
+    """We test it as normal student, not superuser to check permissions
+    """
+    api.user.is_superuser = False
+    api.user.save()
+
+    return api
+
+
+@pytest.fixture
+def course(mixer):
+    return mixer.blend('products.Course')
+
+
+@pytest.fixture(autouse=True)
+def order(factory, course, api):
+    return factory.order(
+        user=api.user,
+        item=course,
+        is_paid=True,
+    )
+
+
+@pytest.fixture
+def unpaid_order(order):
+    order.set_not_paid()
+
+    return order
+
+
+@pytest.fixture(autouse=True)
+def material(mixer, course):
+    return mixer.blend('notion.Material', course=course, page_id='0e5693d2-173a-4f77-ae81-06813b6e5329')
+
+
+@pytest.fixture
+def page() -> NotionPage:
+    return NotionPage(blocks=NotionBlockList([
+        NotionBlock(id='block-1', data={'block-name': 'block-1'}),
+        NotionBlock(id='block-2', data={'block-name': 'block-2'}),
+    ]))
+
+
+@pytest.fixture(autouse=True)
+def fetch_page_recursively(mocker, page: NotionPage):
+    return mocker.patch('notion.client.NotionClient.fetch_page_recursively', return_value=page)
