9fd7932d4df33086e424bad17ea4f7fb99bc8349
Fedor Borshev <fedor@borshev.com>
2022-03-24 20:46:13 +0300

    Finaly enabled stripe!

--- src/orders/signals/handlers.py
+++ src/orders/signals/handlers.py
@@ -2,12 +2,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from orders.models import Order
+from stripebank.models import StripeNotification
 from tinkoff.models import CreditNotification as TinkoffCreditNotification
 from tinkoff.models import PaymentNotification as TinkoffPaymentNotification
 
 
 @receiver(post_save, sender=TinkoffPaymentNotification)
-def mark_order_as_payd_on_tinkoff_bank_transactions(instance, created, **kwargs):
+def mark_order_as_paid_on_tinkoff_bank_transactions(instance: TinkoffPaymentNotification, created: bool, **kwargs):
     if not created:
         return
 
@@ -19,7 +20,7 @@ def mark_order_as_payd_on_tinkoff_bank_transactions(instance, created, **kwargs)
 
 
 @receiver(post_save, sender=TinkoffCreditNotification)
-def mark_order_as_payd_on_tinkoff_credit_transactions(instance, created, **kwargs):
+def mark_order_as_paid_on_tinkoff_credit_transactions(instance: TinkoffCreditNotification, created: bool, **kwargs):
     if not created:
         return
 
@@ -27,4 +28,16 @@ def mark_order_as_payd_on_tinkoff_credit_transactions(instance, created, **kwarg
         return
 
     order = Order.objects.get(pk=instance.order_id)
+    order.set_paid()
+
+
+@receiver(post_save, sender=StripeNotification)
+def mark_order_as_paid_on_stripe_notifications(instance: StripeNotification, created: bool, **kwargs):
+    if not created:
+        return
+
+    if instance.status != 'complete':
+        return
+
+    order = Order.objects.get(pk=instance.order_id.replace('tds-', ''))
     order.set_paid()
---
+++ src/orders/tests/pay_signals/test_setting_order_as_paid_on_stripe_bank_transaction.py
@@ -0,0 +1,40 @@
+import json
+import pytest
+from datetime import datetime
+
+pytestmark = [
+    pytest.mark.django_db,
+    pytest.mark.freeze_time('2032-12-01 15:30'),
+]
+
+
+@pytest.fixture
+def webhook(order):
+    with open('./stripebank/tests/.fixtures/webhook.json', 'r') as fp:
+        bank_data = json.load(fp)
+        bank_data['data']['object']['client_reference_id'] = f'tds-{order.pk}'
+
+        return bank_data
+
+
+@pytest.fixture(autouse=True)
+def _disable_signature_verification(mocker):
+    mocker.patch('stripe.webhook.WebhookSignature.verify_header', return_value=True)
+
+
+def test(anon, webhook, order):
+    anon.post('/api/v2/banking/stripe-webhooks/', webhook, expected_status_code=200)
+
+    order.refresh_from_db()
+
+    assert order.paid == datetime(2032, 12, 1, 15, 30)
+
+
+@pytest.mark.parametrize('status', ['not-complete', 'f4ke'])
+def test_wrong_status(anon, order, webhook, status):
+    webhook['data']['object']['status'] = status
+    anon.post('/api/v2/banking/stripe-webhooks/', webhook, expected_status_code=200)
+
+    order.refresh_from_db()
+
+    assert order.paid is None
