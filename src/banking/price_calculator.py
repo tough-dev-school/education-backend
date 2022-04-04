cb41e60d186e79fb481370838e2f57b84c56dc7e
Fedor Borshev <fedor@borshev.com>
2022-04-04 20:17:54 +0300

    Introducing ue_rate_by_bank_name()

--- src/banking/price_calculator.py
+++ src/banking/price_calculator.py
@@ -3,6 +3,7 @@ from typing import Type
 from decimal import Decimal
 
 from banking.base import Bank
+from banking.selector import BankSelector
 
 
 def to_bank(bank: Type[Bank], price: Decimal) -> Decimal:
@@ -12,3 +13,12 @@ def to_bank(bank: Type[Bank], price: Decimal) -> Decimal:
         exchanged_price = Decimal(round(exchanged_price))
 
     return Decimal(round(exchanged_price, 2))
+
+
+def ue_rate_by_bank_name(bank_name: str) -> int:
+    if bank_name not in BankSelector.banks:
+        raise KeyError(f'Bank not found: {bank_name}')
+
+    Bank = BankSelector()(bank_name)
+
+    return Bank.ue
