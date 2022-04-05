cbe8e766833a11b54a016f088ab2fb40ca81b823
TvojaMamaLama <aleshnikov9@gmail.com>
2022-04-05 12:53:16 +0300

    delete env

--- src/app/conf/email.py
+++ src/app/conf/email.py
@@ -4,8 +4,8 @@ EMAIL_ENABLED = env('EMAIL_ENABLED', cast=bool, default=False)
 
 EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
 
-DASHAMAIL_API_KEY = env('DASHAMAIL_API_KEY', default='3f151d7fef15b61312d1c334e296c90f')
-DASHAMAIL_LIST_ID = env('DASHAMAIL_LIST_ID', default='79357')
+DASHAMAIL_API_KEY = env('DASHAMAIL_API_KEY', default='')
+DASHAMAIL_LIST_ID = env('DASHAMAIL_LIST_ID', default='')
 
 DEFAULT_FROM_EMAIL = env('EMAIL_FROM', cast=str, default='')
 ANYMAIL = {
