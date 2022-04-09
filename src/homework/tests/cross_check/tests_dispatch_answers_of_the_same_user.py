38f770c38c6ec94e96f5a6ff627cc51144c0dc44
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-04-09 17:21:36 +0300

    Rename fixtures and use mixer.cycle

--- src/homework/tests/cross_check/tests_dispatch_answers_of_the_same_user.py
+++ src/homework/tests/cross_check/tests_dispatch_answers_of_the_same_user.py
@@ -8,10 +8,7 @@ pytestmark = [
 
 @pytest.fixture
 def answers(mixer, question, user):
-    return [
-        mixer.blend('homework.Answer', question=question, author=user, parent=None),
-        mixer.blend('homework.Answer', question=question, author=user, parent=None),
-    ]
+    return mixer.cycle(2).blend('homework.Answer', question=question, author=user, parent=None)
 
 
 @pytest.fixture
@@ -20,25 +17,25 @@ def service(dispatcher, answers):
 
 
 @pytest.fixture
-def get(service):
-    def _get(user):
+def get_answer_to_check(service):
+    def _get_answer_to_check(user):
         return service.get_answer_to_check(user)
 
-    return _get
+    return _get_answer_to_check
 
 
 @pytest.fixture
-def give(service):
-    def _give(user, answer):
+def give_answer_to_user(service):
+    def _give_answer_to_user(answer, user):
         return service.give_answer_to_user(answer, user)
 
-    return _give
+    return _give_answer_to_user
 
 
-def test_first_answer_is_got(get, answers, another_user):
-    assert get(another_user) == answers[0]
+def test_first_answer_is_got(get_answer_to_check, answers, another_user):
+    assert get_answer_to_check(another_user) == answers[0]
 
 
-def test_other_answers_are_ignored(get, give, another_user):
-    _ = give(another_user, get(another_user))
-    assert get(another_user) is None
+def test_other_answers_are_ignored(get_answer_to_check, give_answer_to_user, another_user):
+    _ = give_answer_to_user(get_answer_to_check(another_user), another_user)
+    assert get_answer_to_check(another_user) is None
