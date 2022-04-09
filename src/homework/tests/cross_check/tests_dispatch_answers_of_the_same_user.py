6d5e976715dabff26a8d1e91e25b0afc970d09ab
Oskar Sharipov <oskarsh[at]riseup[dot]net>
2022-04-09 18:07:30 +0300

    Use dispatcher.__call__ in test_other_answers_are_ignored

--- src/homework/tests/cross_check/tests_dispatch_answers_of_the_same_user.py
+++ src/homework/tests/cross_check/tests_dispatch_answers_of_the_same_user.py
@@ -1,5 +1,7 @@
 import pytest
 
+from homework.models import AnswerCrossCheck
+
 pytestmark = [
     pytest.mark.django_db,
     pytest.mark.repeat(10),  # we have to repeat this test suite because the method does randomizing
@@ -7,35 +9,30 @@ pytestmark = [
 
 
 @pytest.fixture
-def answers(mixer, question, user):
-    return mixer.cycle(2).blend('homework.Answer', question=question, author=user, parent=None)
+def answers(mixer, question, user, another_user):
+    return mixer.cycle(2).blend('homework.Answer', question=question, author=user, parent=None) \
+        + [mixer.blend('homework.Answer', question=question, author=another_user, parent=None)]
 
 
 @pytest.fixture
-def service(dispatcher, answers):
+def dispatcher(dispatcher, answers):
     return dispatcher(answers=answers)
 
 
 @pytest.fixture
-def get_answer_to_check(service):
+def get_answer_to_check(dispatcher):
     def _get_answer_to_check(user):
-        return service.get_answer_to_check(user)
+        return dispatcher.get_answer_to_check(user)
 
     return _get_answer_to_check
-
-
-@pytest.fixture
-def give_answer_to_user(service):
-    def _give_answer_to_user(answer, user):
-        return service.give_answer_to_user(answer, user)
-
-    return _give_answer_to_user
 
 
 def test_first_answer_is_got(get_answer_to_check, answers, another_user):
     assert get_answer_to_check(another_user) == answers[0]
 
 
-def test_other_answers_are_ignored(get_answer_to_check, give_answer_to_user, another_user):
-    _ = give_answer_to_user(get_answer_to_check(another_user), another_user)
-    assert get_answer_to_check(another_user) is None
+def test_other_answers_are_ignored(dispatcher, answers):
+    dispatcher()
+
+    assert AnswerCrossCheck.objects.filter(answer=answers[0]).exists()
+    assert not AnswerCrossCheck.objects.filter(answer=answers[1]).exists()
