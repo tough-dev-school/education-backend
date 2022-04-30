99a5ba8bd600d313474b1899a2eee8b9dd5ef33b
Fedor Borshev <fedor@borshev.com>
2022-04-30 14:03:15 +0300

    bank refactoring: dropped unneeded helper

--- src/banking/price_calculator.py
+++ src/banking/price_calculator.py
@@ -1,24 +1,15 @@
-from typing import Optional, Type
+from typing import Type
 
 from decimal import Decimal
 
 from banking.base import Bank
-from banking.selector import BankSelector
 
 
 def to_bank(bank: Type[Bank], price: Decimal) -> Decimal:
+    """Get sum in bank currency"""
     exchanged_price = Decimal(price / bank.ue)
 
     if not price % 1:  # initial price contains decimal part, e.g. kopecks
         exchanged_price = Decimal(round(exchanged_price))
 
     return Decimal(round(exchanged_price, 2))
-
-
-def ue_rate_by_bank_name(bank_name: Optional[str]) -> int:
-    if bank_name not in ['', None, *BankSelector.banks.keys()]:
-        raise KeyError(f'Bank not found: {bank_name}')
-
-    Bank = BankSelector()(bank_name)
-
-    return Bank.ue
