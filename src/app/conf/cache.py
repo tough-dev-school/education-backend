f51114309054b214910ea7742c7c7040988b42f5
Nikolay Kiryanov <nikolay@kiryanov.ru>
2022-11-24 18:14:55 +0300

    Explicitly disable `django-cachalot`when `NO_CACHE` is turned on

--- src/app/conf/cache.py
+++ src/app/conf/cache.py
@@ -1,15 +1,17 @@
 from app.conf.environ import env
 
-CACHES = {
-    'default': env.cache('REDISCLOUD_URL'),
-}
-
 if env('NO_CACHE', cast=bool, default=False):
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
         },
     }
+    CACHALOT_ENABLED = False  # Just disabled explicitly. If enabled here it will not work cause the cache backend is DummyCache.
+else:
+    CACHES = {
+        'default': env.cache('REDISCLOUD_URL'),
+    }
+
 
 if env('CI', cast=bool, default=False):
     CACHALOT_ENABLED = False  # disable `django-cachalot` in tests https://github.com/noripyt/django-cachalot/issues/126
