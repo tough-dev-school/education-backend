a10ac999d46fc8b6dad3ea0345dd3d1e5b47c02f
Nikolay Kiryanov <nkiryanov@fands.dev>
2022-11-24 18:25:00 +0300

    Disable cache for sure, disable `django-cashalot` when `CI` is True (#1362)
    * Cashing settings improvements:
    1. `NO_CACHE` disables cache completely
    2. Explicitly disable `cachalot` in `CI` (for tests purpose)

--- src/app/conf/cache.py
+++ src/app/conf/cache.py
@@ -1,13 +1,22 @@
 from app.conf.environ import env
 
-if not env('NO_CACHE', cast=bool, default=False):
+if env('NO_CACHE', cast=bool, default=False):
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        },
+    }
+    CACHALOT_ENABLED = False  # Just disabled explicitly. If enabled here it will not work cause the cache backend is DummyCache.
+else:
     CACHES = {
         'default': env.cache('REDISCLOUD_URL'),
     }
 
+
+if env('CI', cast=bool, default=False):
+    CACHALOT_ENABLED = False  # disable `django-cachalot` in tests https://github.com/noripyt/django-cachalot/issues/126
+
 CACHALOT_UNCACHABLE_TABLES = [
     'django_migrations',
     'django_content_type',
-    'orders_order',  # https://github.com/noripyt/django-cachalot/issues/126
-    'chains_progress',
 ]
