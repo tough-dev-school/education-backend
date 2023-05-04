b0808332520ab3107b8b6cb38a2bded4e9a32088
Fedor Borshev <1592663+f213@users.noreply.github.com>
2023-05-04 10:49:28 +0300

    Better Sentry instrumentation (#1590)
    * Added profiling and httpx integration
    * CI is now notifying sentry about new releases

--- src/app/conf/sentry.py
+++ src/app/conf/sentry.py
@@ -6,11 +6,13 @@ if not env("DEBUG") and SENTRY_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.celery import CeleryIntegration
     from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk.integrations.httpx import HttpxIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
-        integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration()],
-        traces_sample_rate=0,
+        integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration(), HttpxIntegration()],
+        traces_sample_rate=0.8,
+        profiles_sample_rate=0.5,
         send_default_pii=True,
     )
