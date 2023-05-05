80276298bb290d4932543e97903db3fe0c12d14a
Fedor Borshev <fedor@borshev.com>
2023-05-05 16:28:21 +0300

    Sentry: excluding boilerplate transactions

--- src/app/conf/sentry.py
+++ src/app/conf/sentry.py
@@ -9,10 +9,24 @@ if not env("DEBUG") and SENTRY_DSN:
     from sentry_sdk.integrations.httpx import HttpxIntegration
     from sentry_sdk.integrations.redis import RedisIntegration
 
+    def strip_transactions(event, hint):
+        if event["transaction"] in (
+            "/api/v2/healthchecks/",
+            "chains.tasks.send_chain_messages",
+            "chains.tasks.send_active_chains",
+            "triggers.tasks.run_started_purchase_trigger",
+            "triggers.tasks.check_for_started_purchase_triggers",
+            "triggers.tasks.check_for_record_feedback_triggers",
+            "/admin/jsi18n/",
+        ):
+            return None
+
+        return event
+
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration(), CeleryIntegration(), RedisIntegration(), HttpxIntegration()],
         traces_sample_rate=0.8,
-        profiles_sample_rate=0.5,
         send_default_pii=True,
+        before_send_transaction=strip_transactions,
     )
