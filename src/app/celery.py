e2c675f7b747f23920106434994c11e3ae35b1b1
Fedor Borshev <fedor@borshev.com>
2023-05-07 21:07:24 +0300

    Dropped the triggers app -- will replace it by external service soon

--- src/app/celery.py
+++ src/app/celery.py
@@ -25,14 +25,6 @@ celery.conf.update(
     timezone=env("TIME_ZONE", cast=str, default="Europe/Moscow"),
     enable_utc=False,
     beat_schedule={
-        "run_started_purchase_trigger": {
-            "task": "triggers.tasks.check_for_started_purchase_triggers",
-            "schedule": crontab(hour="*", minute=15),
-        },
-        "run_record_feedback_trigger": {
-            "task": "triggers.tasks.check_for_record_feedback_triggers",
-            "schedule": crontab(hour="*", minute=15),
-        },
         "ship_unshipped_orders": {
             "task": "orders.tasks.ship_unshipped_orders",
             "schedule": crontab(hour="*", minute=0),
