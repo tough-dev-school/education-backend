9efcf7564935b4f68d5888b4ed2252d13ca90cb8
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-06-20 19:54:27 +0500

    add BaseService and AppServiceException

---
+++ src/app/exceptions.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+from rest_framework.response import Response
+from rest_framework.views import exception_handler
+import sentry_sdk
+
+
+class AppServiceException(Exception):
+    """Inherit your custom service exceptions from this class."""
+
+
+def app_service_exception_handler(exc: Exception, context: dict) -> Optional[Response]:
+    """
+    Transform service errors to standard 400 errors and
+    Log all DRF exceptions to sentry, including ValidationErrors
+    """
+
+    sentry_sdk.capture_exception(exc)
+
+    if not isinstance(exc, AppServiceException):
+        return exception_handler(exc, context)
+
+    return Response(status=400, data={"serviceError": str(exc)})
