b8e1678403652732a2c068890f6dbbfcf92ac506
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-07-07 13:13:59 +0500

    add slug to Group admin

--- src/products/admin/group.py
+++ src/products/admin/group.py
@@ -5,5 +5,5 @@ from products.models import Group
 
 @admin.register(Group)
 class GroupAdmin(ModelAdmin):
-    list_display = ["id", "name"]
-    list_editable = ["name"]
+    list_display = ["id", "name", "slug"]
+    list_editable = ["name", "slug"]
