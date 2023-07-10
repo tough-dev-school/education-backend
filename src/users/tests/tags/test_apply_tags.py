40a1419612c220ae4ab98eb4ae64a1547c46d403
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-07-10 12:51:19 +0500

    test tags append once

--- src/users/tests/tags/test_apply_tags.py
+++ src/users/tests/tags/test_apply_tags.py
@@ -9,6 +9,14 @@ pytestmark = [pytest.mark.django_db]
 def test_correct_subscribes_to_dashamail(user, mock_subscribe_to_dashamail):
     apply_tags(user)
 
-    user.refresh_from_db()
     assert len(user.tags) > 0
     mock_subscribe_to_dashamail.assert_called_once_with(user=user, tags=user.tags)
+
+
+@pytest.mark.usefixtures("paid_order", "unpaid_order")
+def test_tags_append_once(user):
+    apply_tags(user)
+    apply_tags(user)
+
+    assert len(user.tags) > 0
+    assert len(user.tags) == len(set(user.tags))
