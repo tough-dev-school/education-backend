f6d80469ceafc6a207b631dd4ac64b3519249582
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-07-12 15:03:27 +0500

    add test for recently added domain

--- src/users/tests/tags/test_b2b_tag.py
+++ src/users/tests/tags/test_b2b_tag.py
@@ -5,8 +5,24 @@ from users.tags.pipeline import apply_tags
 pytestmark = [pytest.mark.django_db]
 
 
+@pytest.fixture
+def new_personal_domain(mixer):
+    return lambda name: mixer.blend("mailing.PersonalEmailDomain", name=name)
+
+
+@pytest.mark.parametrize("domain", ["hehe.xD", "not.company.yep", "pepe.friends"])
+def test_with_recently_added_personal_domain(user, new_personal_domain, domain):
+    new_personal_domain(domain)
+    user.email = f"myemail@{domain}"
+    user.save()
+
+    apply_tags(user)
+
+    assert "b2b" not in user.tags
+
+
 @pytest.mark.parametrize("domain", ["mail.ru", "gmail.com", "yandex.ru"])
-def test_with_common_domain(user, domain):
+def test_with_default_personal_domains(user, domain):
     user.email = f"something@{domain}"
     user.save()
 
