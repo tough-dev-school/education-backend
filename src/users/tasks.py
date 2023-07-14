5f21057138386cf147954775295a097bdc8cd4ee
Daniil Maltsev <56772947+da-maltsev@users.noreply.github.com>
2023-07-14 19:30:40 +0500

    add task and command for updating all students tags (#1763)
    * add task and command for updating all students tags
    
    * drop task for all just call a task for each student

--- src/users/tasks.py
+++ src/users/tasks.py
@@ -4,7 +4,7 @@ from django.apps import apps
 
 from app.celery import celery
 from app.tasks import update_dashamail_subscription
-from users.tags.pipeline import apply_tags
+from users.tags.pipeline import generate_tags
 
 
 @celery.task(name="users.rebuild_tags")
@@ -12,5 +12,5 @@ def rebuild_tags(student_id: str | int, list_id: str | None = None) -> None:
     time.sleep(1)  # preventing race condition when task looks for user which isn't saved into db yet
     student = apps.get_model("users.Student").objects.get(pk=student_id)
 
-    apply_tags(student)
+    generate_tags(student)
     update_dashamail_subscription.delay(user_id=student.pk, list_id=list_id)
--- src/users/tests/tags/test_rebuild_tags_task.py
+++ src/users/tests/tags/test_rebuild_tags_task.py
@@ -13,17 +13,17 @@ def update_subscription(mocker):
 
 
 @pytest.fixture(autouse=True)
-def apply_tags(mocker):
-    return mocker.patch("users.tasks.apply_tags")
+def generate_tags(mocker):
+    return mocker.patch("users.tasks.generate_tags")
 
 
-def test_task(user, apply_tags, update_subscription):
+def test_task(user, generate_tags, update_subscription):
     tasks.rebuild_tags.delay(
         student_id=user.id,
         list_id="1",
     )
 
-    apply_tags.assert_called_once_with(user)
+    generate_tags.assert_called_once_with(user)
     update_subscription.assert_called_once_with(
         list_id="1",
         user_id=user.pk,
