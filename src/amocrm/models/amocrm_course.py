7fad193a53fbafb1e22bb4e0b6cf358b40fe17ed
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-07-31 17:16:46 +0500

    add related_name to crm model

--- src/amocrm/models/amocrm_course.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from app.models import TimestampedModel
-
-
-class AmoCRMCourse(TimestampedModel):
-    course = models.OneToOneField("products.Course", on_delete=models.PROTECT)
-    amocrm_id = models.PositiveIntegerField(null=False, blank=False, unique=True)
