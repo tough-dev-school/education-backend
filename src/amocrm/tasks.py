90a27d7f59f8bc45406e5f029d1a9ac6784d6d7d
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-07-31 16:54:57 +0500

    add tasks, command and specific queue for celery

--- src/amocrm/tasks.py
+++ src/amocrm/tasks.py
@@ -1,51 +1,18 @@
-from httpx import TransportError
-
 from amocrm.client import AmoCRMClient
-from amocrm.client.http import AmoCRMClientException
 from amocrm.models import AmoCRMUser
-from amocrm.services.access_token_getter import AmoCRMTokenGetterException
 from app.celery import celery
 from users.models import User
 
 client = AmoCRMClient()
 
 
-@celery.task(
-    autoretry_for=[TransportError, AmoCRMTokenGetterException, AmoCRMClientException],
-    retry_kwargs={
-        "max_retries": 10,
-        "countdown": 1,
-    },
-    rate_limit="3/s",
-    acks_late=True,
-)
-def enable_customers() -> None:
-    client.enable_customers()
-
-
-@celery.task(
-    autoretry_for=[TransportError, AmoCRMTokenGetterException, AmoCRMClientException],
-    retry_kwargs={
-        "max_retries": 10,
-        "countdown": 1,
-    },
-    rate_limit="3/s",
-    acks_late=True,
-)
+@celery.task
 def create_customer(user_id: int) -> int:
     user = User.objects.get(id=user_id)
     return client.create_customer(user=user)
 
 
-@celery.task(
-    autoretry_for=[TransportError, AmoCRMTokenGetterException, AmoCRMClientException],
-    retry_kwargs={
-        "max_retries": 10,
-        "countdown": 1,
-    },
-    rate_limit="3/s",
-    acks_late=True,
-)
+@celery.task
 def update_customer(amocrm_user_id: int) -> int:
     amocrm_user = AmoCRMUser.objects.get(amocrm_id=amocrm_user_id)
     return client.update_customer(amocrm_user=amocrm_user)
