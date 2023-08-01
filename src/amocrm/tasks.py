1c0fd6dcb31e44f46d1513a0a0cfcc57d83446ae
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-08-01 18:05:42 +0500

    just initiate client in task

--- src/amocrm/tasks.py
+++ src/amocrm/tasks.py
@@ -14,10 +14,6 @@ def amocrm_enabled() -> bool:
     return settings.AMOCRM_BASE_URL != ""
 
 
-def get_client() -> AmoCRMClient:
-    return AmoCRMClient()
-
-
 @celery.task(
     autoretry_for=[TransportError, AmoCRMTokenGetterException, AmoCRMClientException],
     retry_kwargs={
@@ -28,7 +24,7 @@ def get_client() -> AmoCRMClient:
     acks_late=True,
 )
 def enable_customers() -> None:
-    client = get_client()
+    client = AmoCRMClient()
     client.enable_customers()
 
 
@@ -42,7 +38,7 @@ def enable_customers() -> None:
     acks_late=True,
 )
 def create_customer(user_id: int) -> int:
-    client = get_client()
+    client = AmoCRMClient()
     user = User.objects.get(id=user_id)
     return client.create_customer(user=user)
 
@@ -57,6 +53,6 @@ def create_customer(user_id: int) -> int:
     acks_late=True,
 )
 def update_customer(amocrm_user_id: int) -> int:
-    client = get_client()
+    client = AmoCRMClient()
     amocrm_user = AmoCRMUser.objects.get(amocrm_id=amocrm_user_id)
     return client.update_customer(amocrm_user=amocrm_user)
