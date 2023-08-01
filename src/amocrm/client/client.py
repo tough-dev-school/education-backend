5568ae69a35c6d9db5148fd144cae3d04ee6f009
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-08-01 19:59:42 +0500

    fix after merge conflict

--- src/amocrm/client/client.py
+++ src/amocrm/client/client.py
@@ -37,36 +37,6 @@ class AmoCRMClient:
         """Enable customers list is required to create/update customers"""
         self.http.patch(url="/api/v4/customers/mode", data={"mode": "segments", "is_enabled": True})
 
-    def refresh_tokens(self) -> None:
-        """Refresh auth tokens"""
-        response = self.http.post(
-            url="/oauth2/access_token",
-            data={
-                "client_id": settings.AMOCRM_INTEGRATION_ID,
-                "client_secret": settings.AMOCRM_CLIENT_SECRET,
-                "grant_type": "refresh_token",
-                "code": cache.get("amocrm_refresh_token"),
-                "redirect_uri": settings.AMOCRM_REDIRECT_URL,
-            },
-        )
-
-        timeout = int(got["expires_in"]) - 60 * 5  # refresh tokens 5 min before expiration time
-        cache.set("amocrm_access_token", got["access_token"], timeout=timeout)
-        cache.set("amocrm_refresh_token", got["refresh_token"])
-
-    def update_customer(self, amocrm_user: AmoCRMUser) -> int:
-        """Updates existing in amocrm customer and returns amocrm_id"""
-        response = self.http.patch(
-            url="/api/v4/customers",
-            data={"id": amocrm_user.amocrm_id, "name": str(amocrm_user.user), "_embedded": {"tags": [{"name": tag} for tag in amocrm_user.user.tags]}},
-        )
-
-        return response["_embedded"]["customers"][0]["id"]
-
-    def enable_customers(self) -> None:
-        """Enable customers list is required to create/update customers"""
-        self.http.patch(url="/api/v4/customers/mode", data={"mode": "segments", "is_enabled": True})
-
     def get_catalogs(self) -> list[AmoCRMCatalog]:
         """Returns all catalogs from amocrm"""
         response = self.http.get(url="/api/v2/catalogs", params={"limit": 250})  # request max amount of catalogs
