0f0df744533424d9a89d19c943fd5bd4877a121d
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-08-01 11:48:14 +0500

    add AmoCRMGroup and Course

---
+++ src/amocrm/models/amocrm_course.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+from app.models import TimestampedModel
+
+
+class AmoCRMUser(TimestampedModel):
+    user = models.OneToOneField("users.User", on_delete=models.PROTECT, related_name="amocrm_user")
+    amocrm_id = models.PositiveIntegerField(null=False, blank=False, unique=True)
