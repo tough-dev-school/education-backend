75267f996c31f9e74b1de60a9fcf0e18c13923cb
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-08-02 14:54:20 +0500

    add docstring to AmoCRMCourse model

--- src/amocrm/models/amocrm_course.py
+++ src/amocrm/models/amocrm_course.py
@@ -4,5 +4,9 @@ from app.models import TimestampedModel
 
 
 class AmoCRMCourse(TimestampedModel):
+    """
+    Link model for products.models.Course and AmoCRM product catalog entity
+    """
+
     course = models.OneToOneField("products.Course", on_delete=models.PROTECT, related_name="amocrm_course")
     amocrm_id = models.PositiveIntegerField(null=False, blank=False, unique=True)
