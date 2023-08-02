bece2ae59a2580b899254b332e8dfe18978bd31c
Daniil Maltsev <56772947+da-maltsev@users.noreply.github.com>
2023-08-02 16:10:42 +0500

    refactor get method in http (#1830)

--- src/amocrm/models/amocrm_user.py
+++ src/amocrm/models/amocrm_user.py
@@ -4,5 +4,9 @@ from app.models import TimestampedModel
 
 
 class AmoCRMUser(TimestampedModel):
+    """
+    Link model for User and AmoCRM customer
+    """
+
     user = models.OneToOneField("users.User", on_delete=models.PROTECT, related_name="amocrm_user")
     amocrm_id = models.PositiveIntegerField(null=False, blank=False, unique=True)
