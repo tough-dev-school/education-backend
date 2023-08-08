7feb86c2062b4ade4cfdb4343c2767be73626c15
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-08-08 10:38:24 +0500

    add AmoCRMUserCOntact

--- src/amocrm/models/__init__.py
+++ src/amocrm/models/__init__.py
@@ -2,8 +2,10 @@ __all__ = [
     "AmoCRMUser",
     "AmoCRMProductGroup",
     "AmoCRMCourse",
+    "AmoCRMUserContact",
 ]
 
 from amocrm.models.amocrm_course import AmoCRMCourse
 from amocrm.models.amocrm_product_group import AmoCRMProductGroup
 from amocrm.models.amocrm_user import AmoCRMUser
+from amocrm.models.amocrm_user_contact import AmoCRMUserContact
