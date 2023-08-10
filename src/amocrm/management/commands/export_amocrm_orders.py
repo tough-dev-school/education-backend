20786309b26a01fe67ca1171e3c8f6b91e7f2f53
Daniil Maltsev <56772947+da-maltsev@users.noreply.github.com>
2023-08-10 17:49:23 +0500

    push started not sooner than 3 motnh ago orders (#1861)

--- src/amocrm/management/commands/export_amocrm_orders.py
+++ src/amocrm/management/commands/export_amocrm_orders.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
+
 from django.apps import apps
 from django.core.management.base import BaseCommand
 from django.db.models import Q
+from django.utils import timezone
 
 from amocrm.tasks import push_order_to_amocrm
 
@@ -10,12 +13,29 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         Order = apps.get_model("orders.Order")
+        three_months_ago = timezone.now() - timedelta(days=90)
 
-        orders_ids = (
-            Order.objects.filter(~Q(user__email=""), unpaid__isnull=True, user__is_active=True, user__is_staff=False, user__email__isnull=False)
+        paid_orders_ids = (
+            Order.objects.filter(
+                ~Q(user__email=""), unpaid__isnull=True, user__is_active=True, user__is_staff=False, user__email__isnull=False, paid__isnull=False
+            )
             .select_related("user")
             .values_list("id", flat=True)
         )
+        started_orders_ids = (
+            Order.objects.filter(
+                ~Q(user__email=""),
+                unpaid__isnull=True,
+                user__is_active=True,
+                user__is_staff=False,
+                user__email__isnull=False,
+                paid__isnull=True,
+                created__gte=three_months_ago,
+            )
+            .select_related("user")
+            .values_list("id", flat=True)
+        )
+        orders_ids = {*paid_orders_ids, *started_orders_ids}
         for order_id in orders_ids:
             push_order_to_amocrm.delay(order_id=order_id)
 
