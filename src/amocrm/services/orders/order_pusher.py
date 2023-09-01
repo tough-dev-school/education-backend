0f57b838a0b6252874bd5ccf6724614063612a52
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-09-01 14:52:27 +0500

    optimize model saving

--- src/amocrm/services/orders/order_pusher.py
+++ src/amocrm/services/orders/order_pusher.py
@@ -47,15 +47,13 @@ class AmoCRMOrderPusher(BaseService):
 
     def create_lead(self) -> None:
         lead_id = AmoCRMLead(order=self.order).create()
-        self.order.amocrm_lead = AmoCRMOrderLead.objects.create(amocrm_id=lead_id)
-        self.order.save()
+        AmoCRMOrderLead.objects.create(amocrm_id=lead_id, order=self.order)
 
     def create_order(self) -> None:
         AmoCRMLead(order=self.order).update(status="purchased")
         transaction_id = AmoCRMTransaction(order=self.order).create()
 
-        self.order.amocrm_transaction = AmoCRMOrderTransaction.objects.create(amocrm_id=transaction_id)
-        self.order.save()
+        AmoCRMOrderTransaction.objects.create(amocrm_id=transaction_id, order=self.order)
 
     def get_lead(self) -> AmoCRMOrderLead | None:
         """
