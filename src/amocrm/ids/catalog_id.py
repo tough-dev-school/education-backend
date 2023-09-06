707bbe46dc204b86deff5b0418a308f7a0f5386b
Daniil Maltsev <56772947+da-maltsev@users.noreply.github.com>
2023-09-06 20:05:22 +0500

    Update src/amocrm/ids/catalog_id.py
    Co-authored-by: Fedor Borshev <1592663+f213@users.noreply.github.com>

--- src/amocrm/ids/catalog_id.py
+++ src/amocrm/ids/catalog_id.py
@@ -7,6 +7,11 @@ CATALOG_TYPES = Literal["products"]
 
 
 def get_catalog_id(catalog_type: CATALOG_TYPES) -> int:
+  for catalog in for catalog in AmoCRMCatalogs.get():
+    if catalog["type"] == catalog_type:
+      return catalogs[0]["id"]
+  
+  raise AmoCRMCacheException(f"Cannot retrieve {catalog_type} catalog id")
     catalogs = [catalog for catalog in AmoCRMCatalogs.get() if catalog["type"] == catalog_type]
     if len(catalogs) != 1:
         raise AmoCRMCacheException(f"Cannot retrieve {catalog_type} catalog id")
