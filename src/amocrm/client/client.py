3fe6e8d959d0ee5f913b32b1ddbb7b99a6c5528a
Daniil Maltsev <danil.zlatoust9999@gmail.com>
2023-09-06 11:01:38 +0500

    after rebase fixes

--- src/amocrm/client/client.py
+++ src/amocrm/client/client.py
@@ -1,7 +1,5 @@
 from amocrm.client.http import AmoCRMHTTP
-from amocrm.types import AmoCRMCatalogElement
 from amocrm.types import AmoCRMCatalogField
-from amocrm.types import AmoCRMCatalogFieldValue
 
 
 class AmoCRMClient:
@@ -20,33 +18,3 @@ class AmoCRMClient:
     def enable_customers(self) -> None:
         """Enable customers list is required to create/update customers"""
         self.http.patch(url="/api/v4/customers/mode", data={"mode": "segments", "is_enabled": True})
-
-    def update_catalog_field(self, catalog_id: int, field_id: int, field_values: list[AmoCRMCatalogFieldValue]) -> list[AmoCRMCatalogFieldValue]:
-        """
-        Updates catalog field, must be used to create/update selectable field options
-        returns list of AmoCRMCatalogFieldValue, every value has id from amocrm
-        """
-        response = self.http.patch(
-            url=f"/api/v4/catalogs/{catalog_id}/custom_fields",
-            data=[
-                {"id": field_id, "nested": [field_value.to_json() for field_value in field_values]},
-            ],
-        )
-        updated_field = response["_embedded"]["custom_fields"][0]
-        return [AmoCRMCatalogFieldValue.from_json(updated_value) for updated_value in updated_field["nested"]]
-
-    def create_catalog_element(self, catalog_id: int, element: AmoCRMCatalogElement) -> AmoCRMCatalogElement:
-        """Creates catalog element in amocrm and returns it with amocrm_id"""
-        response = self.http.post(
-            url=f"/api/v4/catalogs/{catalog_id}/elements",
-            data=[element.to_json()],
-        )
-        return AmoCRMCatalogElement.from_json(response["_embedded"]["elements"][0])
-
-    def update_catalog_element(self, catalog_id: int, element: AmoCRMCatalogElement) -> AmoCRMCatalogElement:
-        """Updates catalog element in amocrm and returns it with amocrm_id"""
-        response = self.http.patch(
-            url=f"/api/v4/catalogs/{catalog_id}/elements",
-            data=[element.to_json()],
-        )
-        return AmoCRMCatalogElement.from_json(response["_embedded"]["elements"][0])
