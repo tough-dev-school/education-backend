061a75213d2ac33b75726d8f196554cfc2d541f0
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-09-20 15:02:31 +0300

    Introduce `Refund` model

--- src/orders/models/__init__.py
+++ src/orders/models/__init__.py
@@ -1,7 +1,9 @@
 from orders.models.order import Order
 from orders.models.promocode import PromoCode
+from orders.models.refund import Refund
 
 __all__ = [
     "Order",
     "PromoCode",
+    "Refund",
 ]
---
+++ src/orders/models/refund.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+from app.models import TimestampedModel
+
+
+class Refund(TimestampedModel):
+    order = models.OneToOneField("orders.Order", on_delete=models.CASCADE, related_name="refund")
+    author = models.ForeignKey("users.User", on_delete=models.RESTRICT, related_name="order_refunds")
+    bank_confirmation_received = models.BooleanField(default=False, verbose_name="Bank refund confirmation")
+
+    class Meta:
+        verbose_name = "Refund"
+        verbose_name_plural = "Refunds"
+        ordering = ["-created"]
+
+    def __str__(self) -> str:
+        return f"Refund for order #{self.order.pk}, by {self.author}"
