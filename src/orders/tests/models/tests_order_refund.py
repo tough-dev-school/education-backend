0c7205138b7677910bf285408cf37c962f156081
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-09-26 14:24:48 +0300

    Order: `refund` method — better named tests

--- src/orders/tests/models/tests_order_refund.py
+++ src/orders/tests/models/tests_order_refund.py
@@ -13,13 +13,20 @@ def paid_order(factory):
 
 
 @pytest.mark.freeze_time("2022-04-19 19:23Z")
-def test_refund_call_service_and_set_base_attributes(paid_order, mocker):
-    spy_refunder = mocker.spy(OrderRefunder, "__call__")
-
+def test_set_payment_and_shipment_attributes(paid_order):
     paid_order.refund()
 
     paid_order.refresh_from_db()
     assert paid_order.paid is None
     assert paid_order.shipped is None
     assert paid_order.unpaid == datetime(2022, 4, 19, 19, 23, tzinfo=timezone.utc)
+
+
+def test_refund_actually_call_refunder_service(paid_order, mocker):
+    spy_refunder = mocker.spy(OrderRefunder, "__call__")
+
+    paid_order.refund()
+
     spy_refunder.assert_called_once()
+    called_service = spy_refunder.call_args.args[0]
+    assert called_service.order == paid_order
