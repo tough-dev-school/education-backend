f07a3f55ee1f0607b5528a524a50bc90ade5717a
hnthh <anyaagarenko@gmail.com>
2023-10-27 15:53:05 +0300

    fix url in ajax

--- src/diplomas/static/admin/js/get_course_students.js
+++ src/diplomas/static/admin/js/get_course_students.js
@@ -3,14 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
   const studentField = document.querySelector('#id_student');
 
   courseField.addEventListener('change', function () {
-    fetch(`/api/v2/courses/${courseField.value}/students/`)
+    fetch(`/api/v2/users/?course=${courseField.value}`)
         .then(response => response.json())
         .then(data => {
             studentField.innerHTML = '';
             data.forEach(item => {
               const option = document.createElement('option');
               option.value = item.id;
-              option.text = item.username;
+              option.text = item.name;
               studentField.appendChild(option);
             });
         });
