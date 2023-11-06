8a9e918841805beec1ef2717c7322469a4b84821
Anya Agarenko <anyaagarenko@gmail.com>
2023-11-06 21:07:25 +0300

    change the diploma manual creation flow in the admin (#2086)

--- src/apps/diplomas/apps.py
+++ src/apps/diplomas/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class DiplomasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.diplomas"
+
+    verbose_name = _("Diplomas")
+    verbose_name_plural = _("Diplomas")
