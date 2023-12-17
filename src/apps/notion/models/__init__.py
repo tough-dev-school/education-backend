1c0fe0d1edb6de61e7b75f5c8bea42c96d9bcd6e
Fedor Borshev <fedor@borshev.com>
2023-12-17 21:27:12 +0400

    Notion asset model

--- src/apps/notion/models/__init__.py
+++ src/apps/notion/models/__init__.py
@@ -1,8 +1,10 @@
+from apps.notion.models.asset import NotionAsset
 from apps.notion.models.cache_entry import NotionCacheEntry
 from apps.notion.models.material import Material
 from apps.notion.models.material_file import MaterialFile
 
 __all__ = [
+    "NotionAsset",
     "NotionCacheEntry",
     "Material",
     "MaterialFile",
