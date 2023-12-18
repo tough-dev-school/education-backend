16a0b97f5fc70445ad05389b12dddcc37f11f186
Fedor Borshev <fedor@borshev.com>
2023-12-18 11:26:54 +0400

    Better notion tasks naming

--- src/apps/notion/tasks.py
+++ src/apps/notion/tasks.py
@@ -4,14 +4,18 @@ from apps.notion.models import Material
 from core.celery import celery
 
 
-@celery.task(rate_limit="6/m", acks_late=True)
-def update_cache_notion_material(page_id: str) -> None:
+@celery.task(
+    rate_limit="6/m",
+    acks_late=True,
+    name="notion.update_cache",
+)
+def update_cache(page_id: str) -> None:
     page = NotionClient().fetch_page_recursively(page_id)
     cache.set(page_id, page)
 
 
-@celery.task
-def update_cache_for_all_active_notion_materials() -> None:
+@celery.task(name="notion.update_cache_for_all_pages")
+def update_cache_for_all_pages() -> None:
     page_ids = Material.objects.active().values_list("page_id", flat=True).distinct()
     for page_id in page_ids:
-        update_cache_notion_material.delay(page_id)
+        update_cache.delay(page_id=page_id)
