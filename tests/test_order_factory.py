00b4d53315c93897d4221736c1227848e874fd69
hnthh <anyaagarenko@gmail.com>
2023-12-18 17:39:35 +0300

    modify perfomance test

--- tests/test_order_factory.py
+++ tests/test_order_factory.py
@@ -1,9 +1,16 @@
 import pytest
 
+from django.contrib.contenttypes.models import ContentType
+
 
 pytestmark = [pytest.mark.django_db]
 
 
-def test_setting_order_as_paid_does_not_generate_too_much_sql(factory, django_assert_max_num_queries):
-    with django_assert_max_num_queries(12):
+def test_setting_order_as_paid_does_not_generate_too_much_sql(factory, django_assert_num_queries):
+    ContentType.objects.clear_cache()
+
+    with django_assert_num_queries(12):
+        factory.order(is_paid=True)
+
+    with django_assert_num_queries(11):
         factory.order(is_paid=True)
