77a2da1012b142860377c49958361dadba833bc8
Anya Agarenko <anyaagarenko@gmail.com>
2023-12-19 14:20:05 +0300

    remove type hints

--- src/core/management/commands/startapp.py
+++ src/core/management/commands/startapp.py
@@ -1,14 +1,9 @@
-from typing import TYPE_CHECKING
-
 from django.conf import settings
 from django.core.management.commands.startapp import Command as BaseCommand
 
-if TYPE_CHECKING:
-    from typing import Any
-
 
 class Command(BaseCommand):
-    def handle(self, **options: "Any") -> None:  # type: ignore[override]
+    def handle(self, **options) -> None:
         directory = settings.BASE_DIR.parent / "apps" / options["name"]
         directory.mkdir()
 
