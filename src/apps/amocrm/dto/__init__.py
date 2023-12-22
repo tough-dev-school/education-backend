b0787e6e342b363d3801abb439227615fc93ff76
Nikolay Kiryanov <nikolay@kiryanov.ru>
2023-12-22 15:22:08 +0300

    Rename `AmoCRMCustomer` --> `AmoCRMCustomerDTO`

--- src/apps/amocrm/dto/__init__.py
+++ src/apps/amocrm/dto/__init__.py
@@ -1,5 +1,5 @@
 from apps.amocrm.dto.catalogs import AmoCRMCatalogsDTO
-from apps.amocrm.dto.customer import AmoCRMCustomer
+from apps.amocrm.dto.customer import AmoCRMCustomerDTO
 from apps.amocrm.dto.groups import AmoCRMGroups
 from apps.amocrm.dto.lead import AmoCRMLead
 from apps.amocrm.dto.lead_note import AmoCRMLeadNoteDTO
@@ -10,7 +10,7 @@ from apps.amocrm.dto.transaction import AmoCRMTransaction
 from apps.amocrm.dto.user_operator import AmoCRMOperatorDTO
 
 __all__ = [
-    "AmoCRMCustomer",
+    "AmoCRMCustomerDTO",
     "AmoCRMProduct",
     "AmoCRMGroups",
     "AmoCRMLead",
