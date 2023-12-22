56b7094a7d89a33dad47fe3d119a7902ca28de7d
Fedor Borshev <fedor@borshev.com>
2023-12-22 21:25:54 +0300

    Completely disable mypy for dev storage

--- src/core/storages.py
+++ src/core/storages.py
@@ -1,21 +1,16 @@
-from typing import no_type_check
-
+# type: ignore
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class ProdReadOnlyStorage(S3Boto3Storage):
-    @no_type_check
     def exists(self, *args, **kwargs):
         return True
 
-    @no_type_check
     def delete(self, *args, **kwargs):
         return
 
-    @no_type_check
     def size(self, *args, **kwargs):
         return 100500
 
-    @no_type_check
     def save(self, *args, **kwargs):
         return None
