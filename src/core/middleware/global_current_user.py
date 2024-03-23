3011f5d4860ee72b11e6ef7c04562227fd2ce489
hnthh <anyaagarenko@gmail.com>
2024-03-23 19:47:07 +0300

    add tch to ignore

--- src/core/middleware/global_current_user.py
+++ src/core/middleware/global_current_user.py
@@ -1,13 +1,12 @@
-from typing import TYPE_CHECKING, Callable
+from typing import Callable
+
+from django.http import HttpRequest, HttpResponse
 
 from core.current_user import set_current_user, unset_current_user
 
-if TYPE_CHECKING:
-    from django.http import HttpRequest, HttpResponse
 
-
-def set_global_user(get_response: "Callable[[HttpRequest], HttpResponse]") -> "Callable[[HttpRequest], HttpResponse]":
-    def middleware(request: "HttpRequest") -> "HttpResponse":
+def set_global_user(get_response: Callable[[HttpRequest], HttpResponse]) -> Callable[[HttpRequest], HttpResponse]:
+    def middleware(request: HttpRequest) -> HttpResponse:
         set_current_user(request.user)  # type: ignore[arg-type]
 
         response = get_response(request)
