044425a3cb02ab160ba440271defe8d01e7a1417
Anya Agarenko <anyaagarenko@gmail.com>
2024-04-01 12:52:40 +0300

    handle `ANN` ruff rules (#2228)

--- src/core/storages.py
+++ src/core/storages.py
@@ -1,16 +1,18 @@
-# type: ignore
-from storages.backends.s3boto3 import S3Boto3Storage
+from django.core.files import File
+from storages.backends.s3boto3 import S3Boto3Storage  # type: ignore[import-untyped]
 
 
 class ProdReadOnlyStorage(S3Boto3Storage):
-    def exists(self, *args, **kwargs):
+    def exists(self, name: str) -> bool:
+        del name
+
         return True
 
-    def delete(self, *args, **kwargs):
-        return
+    def delete(self, name: str) -> None:
+        del name
 
-    def size(self, *args, **kwargs):
+    def save(self, name: str, content: File, max_length: int | None = None) -> None:
+        del content, name, max_length
+
+    def size(self) -> int:
         return 100500
-
-    def save(self, *args, **kwargs):
-        return None
