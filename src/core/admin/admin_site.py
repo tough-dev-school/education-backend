4d8478892dd1916230cd8e9e21ed8c36ab52e6f8
Alex Ebenrode <alexebenrode@gmail.com>
2024-10-01 17:22:27 +0500

    fix after review

--- src/core/admin/admin_site.py
+++ src/core/admin/admin_site.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from django.contrib import admin
+from django.http.request import HttpRequest
 
 
 class AdminSite(admin.AdminSite):
@@ -8,11 +9,18 @@ class AdminSite(admin.AdminSite):
         super().__init__(name=name)
         self._registry.update(admin.site._registry)
 
-    def get_app_list(self, request: Any, app_label: Any | None = None) -> list[Any]:
+    def get_app_list(self, request: HttpRequest, app_label: str | None = None) -> list[str]:
         app_list = super().get_app_list(request, app_label)
-        app_order = ["orders", "notion", "chains", "products", "otherapp"]
-        app_list.sort(key=lambda x: app_order.index(x["app_label"]) if x["app_label"] in app_order else len(app_order))
+        app_list.sort(key=self._get_app_order_index)
         return app_list
+
+    def _get_app_order_index(self, element: Any) -> int:
+        app_order = ["orders", "notion", "chains", "products", "otherapp"]
+
+        if element["app_label"] in app_order:
+            return app_order.index(element["app_label"])
+
+        return len(app_order)
 
 
 admin_site = AdminSite(name="custom_admin")
