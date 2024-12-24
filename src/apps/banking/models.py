86839dda8dbfcca6d1b81514ed661cb8cc6012ec
Polina Nikitina <paulinenik@fands.dev>
2024-12-24 21:24:14 +0300

    Add currency choices

--- src/apps/banking/models.py
+++ src/apps/banking/models.py
@@ -1,11 +1,18 @@
 from django.db import models
+from django.db.models import TextChoices
 from django.utils.translation import gettext_lazy as _
 
 from core.models import DefaultModel
 
 
 class CurrencyRate(DefaultModel):
-    name = models.CharField(max_length=4, unique=True, help_text=_("Currency name in ISO 4217 format. E.g. USD"))
+    class Currency(TextChoices):
+        RUB = "RUB", _("RUB")
+        USD = "USD", _("USD")
+        KZT = "KZT", _("KZT")
+        KIS = "KIS", _("KIS (for zero-price orders)")
+
+    name = models.CharField(max_length=4, unique=True, choices=Currency.choices, db_index=True)
     rate = models.DecimalField(max_digits=6, decimal_places=2, verbose_name=_("Rate"))
 
     class Meta:
