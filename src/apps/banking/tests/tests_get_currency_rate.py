96c8725cde1ade32c3f28d49e9de9415d38b7310
Polina Nikitina <67908968+paulinenik@users.noreply.github.com>
2024-12-26 19:47:15 +0300

    Fix tests (#2485)

--- src/apps/banking/tests/tests_get_currency_rate.py
+++ src/apps/banking/tests/tests_get_currency_rate.py
@@ -2,16 +2,20 @@ import pytest
 
 from apps.banking.base import Bank
 from apps.banking.exceptions import CurrencyRateDoesNotExist
-from apps.banking.models import CurrencyRate
 
 pytestmark = [pytest.mark.django_db]
 
 
+@pytest.fixture
+def rub(factory):
+    return factory.currency_rate(name="RUB", rate=1)
+
+
+@pytest.mark.usefixtures("rub")
 def test_get_currency_rate():
     assert Bank.get_currency_rate() == 1
 
 
 def test_raise_if_no_currency_rate():
-    CurrencyRate.objects.all().delete()
     with pytest.raises(CurrencyRateDoesNotExist):
         Bank.get_currency_rate()
