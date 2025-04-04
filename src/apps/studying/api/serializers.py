fa02e6983e89f248ed0c109895eba3cfc08ebdaa
Fedor Borshev <fedor@borshev.com>
2025-04-04 20:59:51 +0300

    Revert "Changed course id in the API (hope it fits well, @brachkow)"
    This reverts commit c1e1f5cf4c098e9246d34ed2bad6b5b15a82a6fd.

--- src/apps/studying/api/serializers.py
+++ src/apps/studying/api/serializers.py
@@ -4,13 +4,13 @@ from apps.products.models import Course
 
 
 class CourseSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(source="slug")
     home_page_slug = serializers.CharField()
 
     class Meta:
         model = Course
         fields = [
             "id",
+            "slug",
             "name",
             "home_page_slug",
             "cover",
