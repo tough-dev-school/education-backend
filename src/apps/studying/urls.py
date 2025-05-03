367b52e20820c0667f254c974f4f2e82d5c89ee9
Fedor Borshev <fedor@borshev.com>
2025-05-03 14:45:22 +0400

    Purchased courses URL update

--- src/apps/studying/urls.py
+++ src/apps/studying/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from apps.studying.api import views
 
 urlpatterns = [
-    path("purchased/", views.PurchasedCoursesView.as_view()),
+    path("studies/purchased/", views.PurchasedCoursesView.as_view()),  # remove it
+    path("purchased-courses/", views.PurchasedCoursesView.as_view()),
 ]
