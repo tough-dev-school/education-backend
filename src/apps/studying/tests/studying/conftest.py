9d14afaa2f57f2b11cc3f7991b03e5cadcb5b45c
Fedor Borshev <1592663+f213@users.noreply.github.com>
2025-05-14 19:49:43 +0300

    Added iOS and Google calendars to the course metadata (#2614)

--- src/apps/studying/tests/studying/conftest.py
+++ src/apps/studying/tests/studying/conftest.py
@@ -20,7 +20,14 @@ def api(api):
 
 @pytest.fixture
 def course(factory):
-    return factory.course(name="Ихтеология для 5 класса", slug="ichteology", cover="test.gif", calendar="ios://cal", chat="https://t.me/chat")
+    return factory.course(
+        name="Ихтеология для 5 класса",
+        slug="ichteology",
+        cover="test.gif",
+        calendar_ios="ios://cal",
+        calendar_google="google://cal",
+        chat="https://t.me/chat",
+    )
 
 
 @pytest.fixture(autouse=True)
