8a122503daf5c9b7f8730b48cac6e3a1c8ab9a95
Fedor Borshev <fedor@borshev.com>
2025-05-25 13:20:25 +0300

    Removed some middleware for debugging frontend issues

--- src/core/conf/middleware.py
+++ src/core/conf/middleware.py
@@ -1,16 +1,14 @@
 from core.conf.environ import env
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.auth.middleware.RemoteUserMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # "debug_toolbar.middleware.DebugToolbarMiddleware",
     "core.middleware.real_ip.real_ip_middleware",
     "core.middleware.set_user_from_non_django_authentication.JWTAuthMiddleware",
     "core.middleware.set_user_from_non_django_authentication.TokenAuthMiddleware",
