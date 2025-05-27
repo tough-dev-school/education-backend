ba4f3853945ac3eecca25d05b7bd463bf8544a74
Fedor Borshev <fedor@borshev.com>
2025-05-27 19:00:20 +0300

    Free-form links in the course

--- src/apps/lms/models/__init__.py
+++ src/apps/lms/models/__init__.py
@@ -1,6 +1,7 @@
 from apps.lms.models.call import Call
+from apps.lms.models.course_link import CourseLink
 from apps.lms.models.lesson import Lesson
 from apps.lms.models.lms_course import Course
 from apps.lms.models.module import Module
 
-__all__ = ["Call", "Course", "Lesson", "Module"]
+__all__ = ["Call", "Course", "CourseLink", "Lesson", "Module"]
