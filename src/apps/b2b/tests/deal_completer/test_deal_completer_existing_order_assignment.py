2ddf2e025bdec75843a105fd72c377b63b3adf49
Fedor Borshev <fedor@borshev.com>
2025-07-17 20:01:14 +0500

    b2b: ignoring self-created orders when searching for orders to assing to a deal during its completion

--- src/apps/b2b/tests/deal_completer/test_deal_completer_existing_order_assignment.py
+++ src/apps/b2b/tests/deal_completer/test_deal_completer_existing_order_assignment.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from functools import partial
 
 import pytest
 
@@ -7,9 +8,14 @@ from apps.orders.models import Order
 pytestmark = [pytest.mark.django_db]
 
 
-def test_order_data_for_asigned_orders(completer, factory):
+@pytest.fixture
+def order(factory, seller):
+    return partial(factory.order, author=seller)
+
+
+def test_order_data_for_asigned_orders(completer, factory, order):
     deal = factory.deal(student_count=1, price=100)
-    existing_order = factory.order(user=deal.students.first().user, item=deal.course, price=500)
+    existing_order = order(user=deal.students.first().user, item=deal.course, price=500)
 
     completer(deal=deal)()
     existing_order.refresh_from_db()
@@ -20,9 +26,9 @@ def test_order_data_for_asigned_orders(completer, factory):
     assert existing_order.price == Decimal(500), "Price should remain the same"
 
 
-def test_asigned_orders_are_paid(completer, factory):
+def test_asigned_orders_are_paid(completer, factory, order):
     deal = factory.deal(student_count=1)
-    existing_order = factory.order(user=deal.students.first().user, item=deal.course)
+    existing_order = order(user=deal.students.first().user, item=deal.course)
 
     completer(deal=deal)()
     existing_order.refresh_from_db()
@@ -31,33 +37,44 @@ def test_asigned_orders_are_paid(completer, factory):
     assert existing_order.shipped is not None
 
 
-def test_paid_orders_are_ignored_during_order_asignment(completer, factory):
+def test_paid_orders_are_ignored_during_order_asignment(completer, factory, order):
     deal = factory.deal(student_count=1)
-    existing_order = factory.order(user=deal.students.first().user, item=deal.course)
+    existing_order = order(user=deal.students.first().user, item=deal.course)
     existing_order.set_paid()
 
     completer(deal=deal)()
     existing_order.refresh_from_db()
 
-    assert existing_order.deal is None, "Deal has not been changed, hence the order is ignored"
+    assert existing_order.deal is None
 
 
-def test_shipped_but_not_paid_orders_are_not_ignored_during_order_asignment(completer, factory):
+def test_shipped_but_not_paid_orders_are_not_ignored_during_order_asignment(completer, factory, order):
     deal = factory.deal(student_count=1)
-    existing_order = factory.order(user=deal.students.first().user, item=deal.course)
+    existing_order = order(user=deal.students.first().user, item=deal.course)
     existing_order.ship_without_payment()
 
     completer(deal=deal)()
     existing_order.refresh_from_db()
 
-    assert existing_order.deal == deal, "Deal has been changed, hence the order is not ignored"
+    assert existing_order.deal == deal
 
 
-def test_orders_with_another_courses_are_ignored(completer, factory, another_course):
+def test_orders_with_another_courses_are_ignored(completer, factory, order, another_course):
     deal = factory.deal(student_count=1)
-    existing_order = factory.order(user=deal.students.first().user, item=another_course)
+    existing_order = order(user=deal.students.first().user, item=another_course)
 
     completer(deal=deal)()
     existing_order.refresh_from_db()
 
-    assert existing_order.deal is None, "Deal has not been changed, hence the order is ignored"
+    assert existing_order.deal is None
+
+
+def test_self_created_orders_are_ignored(completer, factory):
+    deal = factory.deal(student_count=1)
+    student = deal.students.first().user
+    existing_order = factory.order(user=student, author=student, item=deal.course)
+
+    completer(deal=deal)()
+    existing_order.refresh_from_db()
+
+    assert existing_order.deal is None
