3d50bca6b8e4dcadd5ac2bce504923eeecdbd528
Fedor Borshev <fedor@borshev.com>
2025-07-28 20:24:36 +0300

    Course group anon endpoint for the new jquery basket

--- src/apps/products/urls.py
+++ src/apps/products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.products.api.views import PromocodeView, PurchaseView
+from apps.products.api import views
 
 urlpatterns = [
-    path("courses/<str:slug>/promocode/", PromocodeView.as_view()),
-    path("courses/<str:slug>/purchase/", PurchaseView.as_view()),
+    path("courses/<str:slug>/promocode/", views.PromocodeView.as_view()),
+    path("courses/<str:slug>/purchase/", views.PurchaseView.as_view()),
+    path("course-groups/<str:slug>/courses/", views.ProductGroupView.as_view()),
 ]
