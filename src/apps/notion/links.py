faad0f3cd8445eea7079dd450ceec0997b38bb90
Fedor Borshev <fedor@borshev.com>
2025-10-15 17:31:07 +0300

    Explicit mccabe complexity

--- src/apps/notion/links.py
+++ src/apps/notion/links.py
@@ -1,7 +1,7 @@
 from apps.notion.types import NotionId, TextProperty
 
 
-def get_links(node: TextProperty) -> list[NotionId]:
+def get_links(node: TextProperty) -> list[NotionId]:  # NOQA: C901
     """Recursively find links in the notion block content"""
     links = []
     for child in node:
