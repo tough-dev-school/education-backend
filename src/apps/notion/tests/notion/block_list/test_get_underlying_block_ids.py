d4a930547d5d57f840a3d5a78307cdf635b79c0b
Fedor Borshev <1592663+f213@users.noreply.github.com>
2025-11-13 11:37:43 +0300

    Materials: fixed blocks with a single content item (#2846)

--- src/apps/notion/tests/notion/block_list/test_get_underlying_block_ids.py
+++ src/apps/notion/tests/notion/block_list/test_get_underlying_block_ids.py
@@ -1,4 +1,63 @@
-from apps.notion.block import NotionBlockList
+import pytest
+
+from apps.notion.block import NotionBlock, NotionBlockList
+
+
+@pytest.fixture
+def block_without_content() -> NotionBlock:
+    return NotionBlock(id="without_content", data={})
+
+
+@pytest.fixture
+def block_with_content() -> NotionBlock:
+    return NotionBlock(
+        id="with_content",
+        data={
+            "value": {
+                "content": ["a", "b", "c"],
+                "last_edited_time": 1642356661000,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def block_with_single_content_element() -> NotionBlock:
+    return NotionBlock(
+        id="with_single_content_element",
+        data={
+            "value": {
+                "content": ["a"],
+                "last_edited_time": 1642356661000,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def another_block_with_content() -> NotionBlock:
+    return NotionBlock(
+        id="another_with_content",
+        data={
+            "value": {
+                "content": ["c", "e", "f"],
+                "last_edited_time": 1642356662000,
+            },
+        },
+    )
+
+
+@pytest.fixture
+def page_block() -> NotionBlock:
+    return NotionBlock(
+        id="empty_page_block",
+        data={
+            "value": {
+                "type": "page",
+                "content": ["g", "h", "i"],
+            },
+        },
+    )
 
 
 def test_no_content(block_without_content):
@@ -11,6 +70,12 @@ def test_content(block_with_content, block_without_content):
     block_list = NotionBlockList([block_with_content, block_without_content])
 
     assert block_list.get_underlying_block_ids() == {"a", "b", "c"}
+
+
+def test_single_content_element(block_with_single_content_element):
+    block_list = NotionBlockList([block_with_single_content_element])
+
+    assert block_list.get_underlying_block_ids() == {"a"}
 
 
 def test_block_ids_are_merged(block_with_content, another_block_with_content):
