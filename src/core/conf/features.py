fe6472fd40356b785731ba1a30c17cd446a1aa26
Fedor Borshev <1592663+f213@users.noreply.github.com>
2026-01-26 15:10:56 +0300

    Dropped unused feature flag `DISABLE_NEW_ANSWER_NOTIFICATIONS` (#2906)

--- src/core/conf/features.py
+++ src/core/conf/features.py
@@ -4,6 +4,4 @@ from core.conf.environ import env
 
 DEBUG = env("DEBUG", cast=bool, default=False)
 
-DISABLE_NEW_ANSWER_NOTIFICATIONS = env("DISABLE_NEW_ANSWER_NOTIFICATIONS", cast=bool, default=False)
-
 HOMEWORK_ANSWER_EDIT_PERIOD = timedelta(days=1)
